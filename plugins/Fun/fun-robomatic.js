52c0c3689d3a121df1f4d628913fbb30397c772d
M.U.F.A.R <132034817+Pkok1024@users.noreply.github.com>
2024-04-19 12:29:23 +0700

    improve

--- plugins/Fun/fun-robomatic.js
+++ plugins/Fun/fun-robomatic.js
@@ -1,35 +1,45 @@
 import fetch from 'node-fetch'
 
-let handler = async (m, {
-    conn,
-    args,
-    text
-}) => {
-    if (!text) throw "Input text"
-    const encodedParams = new URLSearchParams();
-    encodedParams.append("in", text);
-    encodedParams.append("op", "in");
-    encodedParams.append("cbot", "1");
-    encodedParams.append("SessionID", "RapidAPI1");
-    encodedParams.append("ChatSource", "RapidAPI");
-    encodedParams.append("cbid", "1");
-    encodedParams.append("key", "RHMN5hnQ4wTYZBGCF3dfxzypt68rVP");
+const apiKey = '414ed3dd91mshcc92bca3c605999p125f24jsnf2650ad70ac3';
+const apiHost = 'robomatic-ai.p.rapidapi.com';
+const apiPath = '/api.php';
 
-    const options = {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/x-www-form-urlencoded',
-            'X-RapidAPI-Host': 'robomatic-ai.p.rapidapi.com',
-            'X-RapidAPI-Key': '414ed3dd91mshcc92bca3c605999p125f24jsnf2650ad70ac3'
-        },
-        body: encodedParams
-    };
+const requestOptions = {
+  method: 'POST',
+  headers: {
+    'content-type': 'application/x-www-form-urlencoded',
+    'X-RapidAPI-Host': apiHost,
+    'X-RapidAPI-Key': apiKey,
+  },
+};
 
-    await fetch('https://robomatic-ai.p.rapidapi.com/api.php', options)
-        .then(res => res.json())
-        .then(res => m.reply(res.out))
-        .catch(e => m.reply(eror));
-}
-handler.command = /^robo$/i
-handler.limit = true
-export default handler
+const handler = async (m, { conn, args, text }) => {
+  if (!text) {
+    throw 'Input text';
+  }
+
+  const encodedParams = new URLSearchParams();
+  encodedParams.append('in', text);
+  encodedParams.append('op', 'in');
+  encodedParams.append('cbot', '1');
+  encodedParams.append('SessionID', 'RapidAPI1');
+  encodedParams.append('ChatSource', 'RapidAPI');
+  encodedParams.append('cbid', '1');
+  encodedParams.append('key', 'RHMN5hnQ4wTYZBGCF3dfxzypt68rVP');
+
+  requestOptions.body = encodedParams;
+
+  try {
+    const response = await fetch(`https://${apiHost}${apiPath}`, requestOptions);
+    const json = await response.json();
+
+    if (!response.ok) {
+      throw new Error(json.message || 'Unexpected error');
+    }
+
+    m.reply(json.out);
+  } catch (e) {
+    m.reply(`An error occurred: ${e.message}`);
+  }
+};
+
