ac574af84c7ba960846be7500b2fa92dd0d3d4fd
M.U.F.A.R <132034817+Pkok1024@users.noreply.github.com>
2024-04-19 12:30:50 +0700

    comment

--- plugins/Others/entertain-meme.js
+++ plugins/Others/entertain-meme.js
@@ -1,43 +1,6 @@
-import fetch from 'node-fetch'
+import fetch from 'node-fetch'  // Importing the 'node-fetch' module to make HTTP requests
 
-let handler = async (m, {
+let handler = async (m, {  // Defining the async handler function with 'm' as the Message object and 'conn' as the connection object
     conn,
     usedPrefix,
-    command
-}) => {
-    try {
-        let anu = await fetch('https://candaan-api.vercel.app/api/image')
-        let json = await anu.json()
-        let fimg = await fetch(json.data[Math.floor(Math.random() * json.data.length)].url)
-        let fimgb = Buffer.from(await fimg.arrayBuffer())
-        if (Buffer.byteLength(fimgb) < 22000) throw new e()
-        await conn.sendFile(m.chat, fimgb, "", '[ RANDOM - ' + command.toUpperCase() + ' ]', m)
-    } catch (e) {
-        try {
-            let anu = await fetch('https://candaan-api.vercel.app/api/image/random')
-            let json = await anu.json()
-            let fimg = await fetch(json.data.url)
-            let fimgb = Buffer.from(await fimg.arrayBuffer())
-            if (Buffer.byteLength(fimgb) < 22000) throw new e()
-            await conn.sendFile(m.chat, fimgb, "", '[ RANDOM - ' + command.toUpperCase() + ' ]', m)
-        } catch (e) {
-            try {
-                let fimg = await fetch('https://api.lolhuman.xyz/api/meme/memeindo?apikey=' + global.lolkey)
-                let fimgb = Buffer.from(await fimg.arrayBuffer())
-                if (Buffer.byteLength(fimgb) < 22000) throw new e()
-                await conn.sendFile(m.chat, fimgb, "", '[ RANDOM - ' + command.toUpperCase() + ' ]', m)
-            } catch (e) {
-                m.reply(`Terjadi kesalahan, coba lagi nanti.`)
-            }
-        }
-    }
-}
 
-handler.help = ['meme']
-handler.tags = ['entertainment']
-handler.command = /^((random)?meme)$/i
-
-handler.premium = false
-handler.limit = true
-
-export default handler
