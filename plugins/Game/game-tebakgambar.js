3ffad47000f38e226d6585a60c2340b3985d5186
M.U.F.A.R <132034817+Pkok1024@users.noreply.github.com>
2024-04-19 12:30:38 +0700

    comment

--- plugins/Game/game-tebakgambar.js
+++ plugins/Game/game-tebakgambar.js
@@ -1,31 +1,35 @@
-import {
-    tebakgambar
-} from '@bochilteam/scraper'
-import {
-    webp2png
-} from '../../lib/webp2mp4.js'
+import { tebakgambar } from '@bochilteam/scraper' // Importing the tebakgambar function from the scraper module
+import { webp2png } from '../../lib/webp2mp4.js' // Importing the webp2png function from the webp2mp4 module
 
-let timeout = 120000
-let poin = 4999
-let handler = async (m, {
-    conn,
-    command,
-    usedPrefix
-}) => {
+let timeout = 120000 // Timeout duration in milliseconds
+let poin = 4999 // Bonus XP points
+
+// Handler function for the tebakgambar command
+const handler = async (m, { conn, command, usedPrefix }) => {
+    // Check if the chat ID is already in the tebakingambar object
     conn.tebakingambar = conn.tebakingambar ? conn.tebakingambar : {}
     let id = m.chat
     if (id in conn.tebakingambar) {
+        // If there is an ongoing game in the chat, send a message and throw an error to stop the current command execution
         conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tebakingambar[id][0])
         throw false
     }
-    let json = await tebakgambar()
-    let caption = `*${command.toUpperCase()}*
+
+    // Fetch a new question using the tebakgambar function
+    const json = await tebakgambar()
+
+    // Generate the caption for the question with the command, timeout, and bonus XP points
+    const caption = `*${command.toUpperCase()}*
 Rangkailah Gambar Ini
 Timeout *${(timeout / 1000).toFixed(2)} detik*
 Ketik ${usedPrefix}hgam untuk bantuan
 Bonus: ${poin} XP
     `.trim()
-    let imgurl = await imageUrl(json.img)
+
+    // Fetch the image URL and convert it to a png format using the webp2png function
+    const imgurl = await imageUrl(json.img)
+
+    // Store the question details, message object, and timeout in the tebakingambar object
     conn.tebakingambar[id] = [
         await conn.sendFile(m.chat, imgurl, '', caption, m),
         json, poin,
@@ -35,25 +39,6 @@ Bonus: ${poin} XP
         }, timeout)
     ]
 }
-handler.help = ['tebakgambar']
-handler.tags = ['game']
-handler.command = /^tebakgambar/i
 
-export default handler
-
-async function imageUrl(url) {
-    try {
-        let Blobs = await (await fetch(url)).blob()
-        let arrayBuffer = await Blobs.arrayBuffer();
-        let buffer = Buffer.from(arrayBuffer);
-        let pngBuffer = await webp2png(buffer);
-        return pngBuffer
-    } catch (error) {
-        console.error("Error:", error);
-    }
-}
-
-const buttons = [
-    ['Hint', '/hgam'],
-    ['Nyerah', 'menyerah']
-]
+// Command information for the handler function
+handler.help = ['tebakg
