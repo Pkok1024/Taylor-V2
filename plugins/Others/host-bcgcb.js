4294ed9873f54aba714d77a59df427dd091cc7d2
M.U.F.A.R <132034817+Pkok1024@users.noreply.github.com>
2024-04-19 12:29:10 +0700

    improve

--- plugins/Others/host-bcgcb.js
+++ plugins/Others/host-bcgcb.js
@@ -1,22 +1,33 @@
 import fs from 'fs'
-let handler = async (m, {
-    conn,
-    args
-}) => {
-    let groups = Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats && !chat.metadata?.read_only && !chat.metadata?.announce).map(v => v[0])
-    let imgr = flaaa.getRandom()
-    let text
-    if (args.length >= 1) {
-        text = args.slice(0).join(" ")
-    } else if (m.quoted && m.quoted.text) {
-        text = m.quoted.text
-    } else throw "Input Teks"
-    for (let id of groups) {
-        let member = (await conn.groupMetadata(id)).participants.map(v => v.jid)
-        await conn.sendFile(m.chat, imgr + 'BROADCAST', '', htki + ' *BROADCAST* ' + htka + '\n\n*Pesan:*\n' + text, m)
-    }
-}
-handler.command = ['bcgcb']
-handler.tags = ['host']
-handler.help = ['bcgcb']
-export default handler
+import fetch from 'node-fetch'
+import { delay } from 'wreckingball-util'
+
+let handler = async (m, { conn, args }) => {
+  if (!conn.user.jid.endsWith('@s.whatsapp.net')) {
+    await m.reply('This command can only be used by a WhatsApp Business account.')
+    return
+  }
+
+  let groups = Object.entries(conn.chats).filter(([jid, chat]) =>
+    jid.endsWith('@g.us') && chat.isChats && !chat.metadata?.read_only && !chat.metadata?.announce
+  ).map(v => v[0])
+
+  if (groups.length === 0) {
+    await m.reply('No groups found.')
+    return
+  }
+
+  let imgr = (await fetch('https://api.lolhuman.xyz/api/random/apikey')).buffer()
+  let text
+  if (args.length >= 1) {
+    text = args.slice(0).join(" ")
+  } else if (m.quoted && m.quoted.text) {
+    text = m.quoted.text
+  } else throw "Input Teks"
+
+  for (let id of groups) {
+    let member = (await conn.groupMetadata(id)).participants.map(v => v.jid)
+    if (!member.includes(conn.user.jid)) continue
+    if (!conn.isAdmin(id)) continue
+
+    await conn.sendFile(m.chat, imgr, '', htki + ' *BROADCAST* ' + htka + '\n\n*Pesan:*\n
