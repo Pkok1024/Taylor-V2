91a4256a33cd9daa7ffae357f9cfd43b15175c06
M.U.F.A.R <132034817+Pkok1024@users.noreply.github.com>
2024-04-19 12:29:39 +0700

    improve

--- plugins/Others/google.js
+++ plugins/Others/google.js
@@ -1,5 +1,3 @@
-import axios from "axios"
-import fetch from "node-fetch"
 import {
     googleIt
 } from "@bochilteam/scraper"
@@ -11,36 +9,34 @@ let handler = async (m, {
     conn,
     args
 }) => {
-    let text
-    if (args.length >= 1) {
-        text = args.slice(0).join(" ")
-    } else if (m.quoted && m.quoted.text) {
-        text = m.quoted.text
-    } else throw "Input teks atau reply teks yang ingin di cari!"
+    let text = args.join(" ")
+    if (!text) throw "Input teks atau reply teks yang ingin di cari!"
     let google_img = "https://telegra.ph/file/cf62f2b8648a352548978.jpg"
     await m.reply(wait)
     try {
         let search = await GoogleIt({
             query: text
         })
-        let caption = search.map((v, index) => `${htki + " " + ++index + " " + htka}\n*${v.title || 'Tidak terdeteksi'}*\n  *○ Link:* ${v.link || 'Tidak terdeteksi'}\n  *○ Snippet:* ${v.snippet || 'Tidak terdeteksi'}`).join("\n\n")
-        await conn.sendFile(m.chat, google_img, "", caption, m)
-    } catch (e) {
-        try {
-            let search = await googleIt(text)
-            let caption = search.articles.map((v, index) => `${htki + " " + ++index + " " + htka}\n*${v.title || 'Tidak terdeteksi'}*\n  *○ Link:* ${v.url || 'Tidak terdeteksi'}\n  *○ Snippet:* ${v.description || 'Tidak terdeteksi'}`).join("\n\n")
-            if (!caption.length) throw `Query "${text}" Not Found`
-            await conn.sendFile(m.chat, google_img, "", caption, m)
-        } catch (e) {
-            try {
+        if (!search.length) {
+            search = await googleIt(text)
+            if (!search.articles.length) {
                 let API_KEY = "7d3eb92cb730ed676d5afbd6c902ac1f"
-                let search = await (await fetch("http://api.serpstack.com/search?access_key=" + API_KEY + "&type=web&query=" + text)).json()
-                let caption = search.organic_results.map((v, index) => `${htki + " " + ++index + " " + htka}\n*${v.title || 'Tidak terdeteksi'}*\n  *○ Link:* ${v.url || 'Tidak terdeteksi'}\n  *○ Snippet:* ${v.snippet || 'Tidak terdeteksi'}`).join("\n\n")
-                await conn.sendFile(m.chat, google_img, "", caption, m)
-            } catch (e) {
-                await m.reply(eror)
+                let res = await fetch(`http://api.serpstack.com/search?access_key=${API_KEY}&type=web&query=${text}`)
+                let json = await res.json()
+                search = json.organic_results
             }
         }
+        let caption = ""
+        search.forEach((v, index) => {
+            let title = v.title || 'Tidak terdeteksi'
+            let url = v.url || 'Tidak terdeteksi'
+            let snippet = v.snippet || 'Tidak terdeteksi'
+            caption += `${htki + " " + ++index + " " + htka}\n*${title}*\n  *○ Link:* ${url}\n  *○ Snippet:* ${snippet}\n`
+        })
+        if (caption) await conn.sendFile(m.chat, google_img, "", caption, m)
+        else throw "Query not found"
+    } catch (e) {
+        await m.reply(eror)
     }
 }
 handler.help = ["google", "googlef"].map(v => v + " <pencarian>")
