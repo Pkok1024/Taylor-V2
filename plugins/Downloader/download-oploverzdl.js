8d2e14d2cf4ce12a7dc5d88fe7a8cb506d6994e0
M.U.F.A.R <132034817+Pkok1024@users.noreply.github.com>
2024-04-19 12:29:17 +0700

    improve

--- plugins/Downloader/download-oploverzdl.js
+++ plugins/Downloader/download-oploverzdl.js
@@ -1,17 +1,15 @@
 import fetch from 'node-fetch'
-let handler = async (m, {
-    conn,
-    args,
-    usedPrefix,
-    command
-}) => {
-    if (!args[0]) throw `Input *URL*`
+import { sticker } from '../lib/sticker.js'
+
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    if (!args[0]) throw `Input *URL*\n\nExample: ${usedPrefix + command} https://...`
     let xyz = "&apikey=wudysoft"
     let wtf = await fetch(`https://xzn.wtf/api/oploverzdl?url=` + args[0] + xyz)
     let fak = await wtf.json()
 
-    let str = `•••••••••••••••••••••••••••••••••••••
+    if (fak.status !== 'Success') throw fak.message
 
+    let str = `
 • Status: ${fak.status}
 • Studio: ${fak.studio}
 • Released: ${fak.released}
@@ -24,45 +22,35 @@ let handler = async (m, {
 • Episode: ${fak.episode}
 • Prev: ${fak.prev}
 • Next: ${fak.next}
+    `.trim()
+
+    let downloads = fak.download
+    for (let i = 0; i < downloads.length; i++) {
+        let download = downloads[i]
+        str += `
+
+•••••••••••••••••••••••••••••••••••••
+• Format: ${download.format}
 
 `
-
-    let a = fak.download
-
-    for (let i = 0; i < a.length; i++) {
-        str += "•••••••••••••••••••••••••••••••••••••\n\n*•• Format: " + a[i].format + '*\n'
-        let b = a[i].resolutions
-        for (let i = 0; i < b.length; i++) {
-            str += "• Resolutions: " + b[i].name + '\n'
-            let c = b[i].servers
-            for (let i = 0; i < c.length; i++) {
-                str += "servers: " + c[i].name + '\n'
-                str += "url: " + c[i].link + '\n\n'
+        let resolutions = download.resolutions
+        for (let j = 0; j < resolutions.length; j++) {
+            let resolution = resolutions[j]
+            str += `• Resolutions: ${resolution.name}\n`
+            let servers = resolution.servers
+            for (let k = 0; k < servers.length; k++) {
+                let server = servers[k]
+                str += `• Server: ${server.name}\n• URL: ${server.link}\n`
             }
         }
     }
 
-    //m.reply(str)
-    conn.relayMessage(m.chat, {
-        extendedTextMessage: {
-            text: str,
-            contextInfo: {
-                externalAdReply: {
-                    title: fak.anime_id + '\n' + wm,
-                    mediaType: 1,
-                    previewType: 0,
-                    renderLargerThumbnail: true,
-                    thumbnailUrl: dua,
-                    sourceUrl: args[0]
-                }
-            },
-        }
-    }, {})
-
+    let stiker = await sticker(false, fak.thumbnail, global.packname, global.author)
+    conn.sendMessage(m.chat, stiker, { quoted: m })
+    conn.reply(m.chat, str, m, { contextInfo: { externalAdReply: { title: fak.anime_id + '\n' + wm, mediaType: 1, previewType: 0, renderLargerThumbnail: true, thumbnailUrl: fak.thumbnail, sourceUrl: args[0] } } })
 }
 
-handler.help = ['oploverz'].map(v => v + ' <url>')
+handler.help = ['oploverz <url>']
 handler.tags = ['downloader']
-handler.command = /^(oploverz|plvrz)$/i
-handler.limit = true
-export default handler
+handler.command = /^(oploverz)$/i
+handler.
