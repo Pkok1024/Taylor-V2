7fb6e67a3f11b2813a617560f4b9b447ab3940e4
M.U.F.A.R <132034817+Pkok1024@users.noreply.github.com>
2024-04-19 12:30:09 +0700

    comment

--- plugins/Image/image-flickr.js
+++ plugins/Image/image-flickr.js
@@ -1,40 +1,48 @@
 import axios from "axios"
+
+// The API key for Flickr API
 const api_Key = "636e1481b4f3c446d26b8eb6ebfe7127";
+
+// The base URL for Flickr API
 const URL = "https://farm66.staticflickr.com";
-let handler = async (m, {
+
+// The function to handle the command
+const handler = async (m, {
     conn,
     isOwner,
     usedPrefix,
     command,
     args
 }) => {
-    let query = "input text\nEx. .flickr hello world\n<command> <tex>"
-    let text
+    // The query to search for on Flickr
+    let query = "input text\nEx. .flickr hello world\n<command> <tex>";
+    
+    // The text to search for
+    let text;
     if (args.length >= 1) {
-        text = args.slice(0).join(" ")
+        // If there are arguments, join them together to form the text
+        text = args.slice(0).join(" ");
     } else if (m.quoted && m.quoted.text) {
-        text = m.quoted.text
-    } else throw query
+        // If there is a quoted message, use its text as the search text
+        text = m.quoted.text;
+    } else {
+        // If there is no text, throw an error with the query syntax
+        throw query;
+    }
 
     try {
-        m.reply(wait)
-        var imge = await searchTopic(text)
-        var rand = imge.getRandom()
-        var resul = "https://farm66.staticflickr.com/" + rand.server + "/" + rand.id + "_" + rand.secret + ".jpg"
-        conn.sendFile(m.chat, resul, "result", "Result Flickr: *" + text.toUpperCase() + "*", m)
+        // Reply to the message with a "wait" message
+        m.reply(wait);
+        
+        // Search for the topic and get a random image
+        var imge = await searchTopic(text);
+        var rand = imge.getRandom();
+        
+        // Construct the URL for the random image
+        var resul = "https://farm66.staticflickr.com/" + rand.server + "/" + rand.id + "_" + rand.secret + ".jpg";
+        
+        // Send the image as a file with a caption
+        conn.sendFile(m.chat, resul, "result", "Result Flickr: *" + text.toUpperCase() + "*", m);
     } catch (e) {
-        throw eror
-    }
-}
-handler.help = ["flickr"]
-handler.tags = ["search"]
-handler.command = /^(flickr)$/i
-export default handler
-
-
-async function searchTopic(query) {
-    const response = await axios.get(
-        `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${api_Key}&tags=${query}&per_page=24&format=json&nojsoncallback=1`
-    )
-    return response.data.photos.photo
-};
+        // If there is an error, throw an error message
+        throw eror;
