f33d213aeb493be7dbf2a1fb6ed8404507574be1
M.U.F.A.R <132034817+Pkok1024@users.noreply.github.com>
2024-04-19 12:30:32 +0700

    comment

--- plugins/Tools/tools-uglify.js
+++ plugins/Tools/tools-uglify.js
@@ -1,53 +1,48 @@
+// Import the 'uglify-js' package to minify and obfuscate JavaScript code
 import uglify from 'uglify-js';
 
-let handler = async (m, {
-    args,
-    command,
-    usedPrefix
+// Define the async function handler to process incoming messages
+const handler = async (m, {
+    args, // The array of arguments passed in the message
+    command, // The command used in the message
+    usedPrefix // The prefix used in the message
 }) => {
+    // Define the usage string for the command
     const usage = `*Example:*
 ${usedPrefix}${command} (Input text or reply text to enc code)
 ${usedPrefix}${command} doc (Reply to a document)`;
 
-    let text;
+    let text; // Declare the variable to store the text to be encrypted
 
+    // Check if arguments are present and join them into a single string
     if (args.length >= 1) {
         text = args.join(" ");
-    } else if (m.quoted && m.quoted.text) {
+    }
+    // If no arguments are present, check if a quoted message is present
+    else if (m.quoted && m.quoted.text) {
         text = m.quoted.text;
-    } else {
+    }
+    // If no quoted message is present, reply with the usage string
+    else {
         return m.reply(usage);
     }
 
     try {
+        // If the text is 'doc' and a quoted document message is present
         if (text === 'doc' && m.quoted && m.quoted.mtype === 'documentMessage') {
-            let docBuffer;
+            let docBuffer; // Declare the variable to store the downloaded document buffer
+            // Download the document and convert it to a string
             if (m.quoted.mimetype) {
                 docBuffer = await m.quoted.download();
             }
+            // Encrypt the document string and send the result as a message
             const message = await Encrypt(docBuffer.toString('utf-8'));
             await m.reply(message);
         } else {
+            // Encrypt the text and send the result as a message
             const message = await Encrypt(text);
             await m.reply(message);
         }
     } catch (error) {
-        const errorMessage = `Terjadi kesalahan: ${error.message}`;
-        await m.reply(errorMessage);
-    }
-
-}
-handler.command = /^(uglify)$/i
-export default handler
-
-async function Encrypt(jsCode) {
-    const result = uglify.minify(jsCode, {
-        mangle: {
-            toplevel: true
-        },
-        compress: {
-            toplevel: true
-        }
-    });
-    return result.code;
-}
+        // If an error occurs, send an error message
+        const errorMessage = `Terj
