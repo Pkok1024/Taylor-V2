ee5ca6d92d23463f8604b1aa3ef3456ab2e62872
M.U.F.A.R <132034817+Pkok1024@users.noreply.github.com>
2024-04-19 12:29:40 +0700

    comment

--- plugins/Bot-function/bot-info.js
+++ plugins/Bot-function/bot-info.js
@@ -1,111 +1,86 @@
+// Import required modules
 import {
-    cpus as _cpus,
-    totalmem,
+    cpus as _cpus, // Import cpus function from 'os' module and alias it as _cpus
+    totalmem, // Import totalmem function from 'os' module
     freemem
 } from 'os'
-import util from 'util'
+import util from 'util' // Import util module
 import {
     performance
-} from 'perf_hooks'
+} from 'perf_hooks' // Import performance module from 'perf_hooks'
 import {
     sizeFormatter
-} from 'human-readable'
+} from 'human-readable' // Import sizeFormatter function from 'human-readable' module
 import {
     join
-} from 'path'
+} from 'path' // Import join function from 'path' module
 import {
     promises
-} from 'fs'
-import moment from 'moment-timezone'
-const more = String.fromCharCode(8206)
-const readMore = more.repeat(4001)
-let format = sizeFormatter({
-    std: 'JEDEC', // 'SI' (default) | 'IEC' | 'JEDEC'
-    decimalPlaces: 2,
-    keepTrailingZeroes: false,
-    render: (literal, symbol) => `${literal} ${symbol}B`,
+} from 'fs' // Import promises object from 'fs' module
+import moment from 'moment-timezone' // Import moment-timezone module
+
+// Define some variables
+const more = String.fromCharCode(8206) // Create a string containing the U+200B character (zero-width space)
+const readMore = more.repeat(4001) // Create a string containing 4001 instances of the U+200B character
+let format = sizeFormatter({ // Create a sizeFormatter object with some options
+    std: 'JEDEC', // Use JEDEC standard for formatting
+    decimalPlaces: 2, // Use 2 decimal places
+    keepTrailingZeroes: false, // Don't keep trailing zeroes
+    render: (literal, symbol) => `${literal} ${symbol}B`, // Use a custom function to render the output
 })
-let handler = async (m, {
-    conn,
-    usedPrefix,
-    __dirname,
-    text,
-    command
+
+// Define the handler function
+let handler = async (m, { // Define the handler function with two parameters: m (the message object) and options (an object containing various options)
+    conn, // The bot's connection object
+    usedPrefix, // The prefix used to call the command
+    __dirname, // The name of the directory containing the current file
+    text, // The text of the message
+    command // The name of the command
 }) => {
-    let date = moment.tz('Asia/Jakarta').format("dddd, Do MMMM, YYYY")
-    let time = moment.tz('Asia/Jakarta').format('HH:mm:ss')
-    let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.json')).catch(_ => ({}))) || {}
-    let _uptime = process.uptime() * 1000
-    let uptime = clockString(_uptime)
-    let totalreg = Object.keys(global.db.data.users).length
-    let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
-    const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
-    const groupsIn = chats.filter(([id]) => id.endsWith('@g.us')) //groups.filter(v => !v.read_only)
-    const used = process.memoryUsage()
-    const cpus = _cpus().map(cpu => {
-        cpu.total = Object.keys(cpu.times).reduce((last, type) => last + cpu.times[type], 0)
-        return cpu
+    // Define some variables
+    let date = moment.tz('Asia/Jakarta').format("dddd, Do MMMM, YYYY") // Get the current date and time in Jakarta time zone
+    let time = moment.tz('Asia/Jakarta').format('HH:mm:ss') // Get the current time in Jakarta time zone
+    let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.json')).catch(_ => ({}))) || {} // Read the package.json file and parse it as JSON
+    let _uptime = process.uptime() * 1000 // Get the uptime of the process in milliseconds
+    let uptime = clockString(_uptime) // Format the uptime using the clockString function
+    let totalreg = Object.keys(global.db.data.users).length // Get the total number of registered users
+    let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length // Get the number of registered users
+    const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats) // Get an array of all chats
+    const groupsIn = chats.filter(([id]) => id.endsWith('@g.us')) // Get an array of all groups
+    const used = process.memoryUsage() // Get the memory usage of the process
+    const cpus = _cpus().map(cpu => { // Get an array of all CPUs and map it to a new array
+        cpu.total = Object.keys(cpu.times).reduce((last, type) => last + cpu.times[type], 0) // Calculate the total CPU time
+        return cpu // Return the modified CPU object
     })
-    const cpu = cpus.reduce((last, cpu, _, {
+    const cpu = cpus.reduce((last, cpu, _, { // Reduce the array of CPUs to a single object
         length
     }) => {
-        last.total += cpu.total
-        last.speed += cpu.speed / length
-        last.times.user += cpu.times.user
-        last.times.nice += cpu.times.nice
-        last.times.sys += cpu.times.sys
-        last.times.idle += cpu.times.idle
-        last.times.irq += cpu.times.irq
-        return last
-    }, {
-        speed: 0,
-        total: 0,
-        times: {
-            user: 0,
-            nice: 0,
-            sys: 0,
-            idle: 0,
-            irq: 0
+        last.total += cpu.total // Add the total CPU time
+        last.speed += cpu.speed / length // Calculate the average CPU speed
+        last.times.user += cpu.times.user // Add the user CPU time
+        last.times.nice += cpu.times.nice // Add the nice CPU time
+        last.times.sys += cpu.times.sys // Add the system CPU time
+        last.times.idle += cpu.times.idle // Add the idle CPU time
+        last.times.irq += cpu.times.irq // Add the IRQ CPU time
+        return last // Return the modified object
+    }, { // Initialize the object with some properties
+        speed: 0, // Initialize the speed property
+        total: 0, // Initialize the total property
+        times: { // Initialize the times object
+            user: 0, // Initialize the user property
+            nice: 0, // Initialize the nice property
+            sys: 0, // Initialize the sys property
+            idle: 0, // Initialize the idle property
+            irq: 0 // Initialize the irq property
         }
     })
-    let old = performance.now()
-    let neww = performance.now()
-    let speed = neww - old
+    let old = performance.now() // Get the current time in milliseconds
+    let neww = performance.now() // Get the current time in milliseconds
+    let speed = neww - old // Calculate the elapsed time
+
+    // Define the capti variable
     let capti = `🤖 ɴᴀᴍᴇ: ${_package.name}
 🧩 ᴠᴇʀsɪᴏɴ: ${_package.version}
 📚 ʟɪʙʀᴀʀʏ: ${_package.description}
 
-⏳ ᴜᴩᴛɪᴍᴇ: ${uptime}
-📈 ᴅᴀᴛᴀʙᴀsᴇ: ${totalreg}
-
-📅 ᴅᴀᴛᴇ: ${date}
-⌚ ᴛɪᴍᴇ: ${time} ﹙ɢᴍᴛ +5:30﹚
-
-💻 sᴇʀᴠᴇʀ ɪɴғᴏ :
-⮕ ᴩɪɴɢ: ${speed} ᴍs
-⮕ ʀᴀᴍ: ${format(totalmem() - freemem())} / ${format(totalmem())}
-
-💬 ᴡʜᴀᴛsᴀᴩᴩ sᴛᴀᴛᴜs :
-⮕ ${groupsIn.length} - Group Chats
-⮕ ${groupsIn.length} - Groups Joined
-⮕ ${groupsIn.length - groupsIn.length} - Groups Left
-⮕ ${chats.length - groupsIn.length} - Personal Chats
-⮕ ${chats.length} - Total Chats
-`.trim()
-
-    await m.reply(capti)
-
-}
-handler.help = ['botinfo']
-handler.tags = ['info']
-handler.command = /^(bot(info)?|infobot)$/i
-
-export default handler
-
-function clockString(ms) {
-    let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000)
-    let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
-    let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-    let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-    return ['\n' + d, ' *Days ☀️*\n ', h, ' *Hours 🕐*\n ', m, ' *Minute ⏰*\n ', s, ' *Second ⏱️* '].map(v => v.toString().padStart(2, 0)).join('')
-}
+⏳ ᴜᴩᴛɪᴍᴇ
