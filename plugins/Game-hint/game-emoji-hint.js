2e774ce5045f9091d0c3d526f35954b25eacee7b
M.U.F.A.R <132034817+Pkok1024@users.noreply.github.com>
2024-04-19 12:29:52 +0700

    improve

--- plugins/Game-hint/game-emoji-hint.js
+++ plugins/Game-hint/game-emoji-hint.js
@@ -1,14 +1,11 @@
-let handler = async (m, {
-    conn
-}) => {
-    conn.tebakemoji = conn.tebakemoji ? conn.tebakemoji : {}
-    let id = m.chat
-    if (!(id in conn.tebakemoji)) throw false
-    let json = conn.tebakemoji[id][1]
-    conn.reply(m.chat, '```' + (json.unicodeName).replace(/[AIUEOaiueo]/ig, '_') + '```', m)
+let handler = async (m, { conn, reply }) => {
+    if (!conn || !conn.tebakemoji || !reply || !m || !m.chat || !m.sender) throw new Error('Invalid or missing properties');
+    let id = m.chat;
+    if (!(id in conn.tebakemoji)) throw new Error('Chat ID not found in tebakemoji object');
+    let json = conn.tebakemoji[id][1];
+    if (!json || !json.unicodeName || typeof json.unicodeName !== 'string' || json.unicodeName.length === 0) throw new Error('JSON object or unicodeName property is invalid');
+    let unicodeName = json.unicodeName.replace(/[AIUEOaiueo]/ig, '_');
+    reply(m.chat, `\`\`\`${unicodeName}\`\`\``, m);
 }
-handler.command = /^hemo$/i
-
-handler.limit = true
-
-export default handler
+handler.command = /^hemo$/i;
+handler.limit = true;
