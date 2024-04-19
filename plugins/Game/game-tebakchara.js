4a689e914d53e3ead0f42227570edc860571b051
M.U.F.A.R <132034817+Pkok1024@users.noreply.github.com>
2024-04-19 12:30:03 +0700

    improve

--- plugins/Game/game-tebakchara.js
+++ plugins/Game/game-tebakchara.js
@@ -1,44 +1,37 @@
 import fetch from 'node-fetch'
-let timeout = 120000
-let poin = 4999
-let handler = async (m, {
-    conn,
-    command,
-    usedPrefix
-}) => {
-    conn.tebakchara = conn.tebakchara ? conn.tebakchara : {}
-    let id = m.chat
+import { poll } from 'games'
+
+const timeout = 120000
+const poin = 4999
+
+const startGame = (conn, m) => {
+    const id = m.chat
     if (id in conn.tebakchara) {
         conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tebakchara[id][0])
         throw false
     }
-    let res = await fetch('https://api.jikan.moe/v4/characters')
-    let jsons = await res.json()
-    let jso = jsons.data
-    let json = jso.getRandom()
-    let caption = `*${command.toUpperCase()}*
-Siapakah nama dari gambar ini
 
-Timeout *${(timeout / 1000).toFixed(2)} detik*
-Ketik ${usedPrefix}hcha untuk hint
-Bonus: ${poin} XP
-    `.trim()
-    conn.tebakchara[id] = [
-        await conn.sendFile(m.chat, json.images.jpg.image_url, '', caption, m),
-        json, poin,
-        setTimeout(() => {
-            if (conn.tebakchara[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.name}*\nKanji : ${json.name_kanji}\n*Url :* ${json.url}\n*Desk :* ${json.about}`, conn.tebakchara[id][0])
-            delete conn.tebakchara[id]
-        }, timeout)
-    ]
-}
-handler.help = ['tebakchara']
-handler.tags = ['game']
-handler.command = /^tebakchara/i
+    return poll(async (m) => {
+        const res = await fetch('https://api.jikan.moe/v4/characters')
+        const jsons = await res.json()
+        const jso = jsons.data
+        const json = jso[Math.floor(Math.random() * jso.length)]
 
-export default handler
+        const caption = `*${m.command.toUpperCase()}*\n\nSiapakah nama dari gambar ini?\n\nTimeout *${(timeout / 1000).toFixed(2)} detik*\nKetik ${m.usedPrefix}hcha untuk hint\nBonus: ${poin} XP`
 
-const buttons = [
-    ['Hint', '/hcha'],
-    ['Nyerah', 'menyerah']
-]
+        conn.tebakchara[id] = [
+            await conn.sendFile(m.chat, json.images.jpg.image_url, '', caption, m),
+            json, poin,
+            setTimeout(() => {
+                if (conn.tebakchara[id]) {
+                    conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.name}*\nKanji : ${json.name_kanji}\n*Url :* ${json.url}\n*Desk :* ${json.about}`, conn.tebakchara[id][0])
+                    delete conn.tebakchara[id]
+                }
+            }, timeout)
+        ]
+    }, {
+        conn,
+        match: /^tebakchara$/i,
+        time: timeout,
+        end: (m) => {
+            if (conn.tebakchara
