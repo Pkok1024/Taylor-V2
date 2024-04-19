4b4493c43b3e07cf9a92da7e6daaea10bd308f2a
M.U.F.A.R <132034817+Pkok1024@users.noreply.github.com>
2024-04-19 12:29:45 +0700

    improve

--- plugins/Group/group-demote.js
+++ plugins/Group/group-demote.js
@@ -1,23 +1,30 @@
 import {
     areJidsSameUser
 } from '@whiskeysockets/baileys'
+
 let handler = async (m, {
     conn,
-    participants
+    participants,
+    isAdmin,
+    isBotAdmin
 }) => {
-    let users = m.mentionedJid.filter(u => !areJidsSameUser(u, conn.user.id))
-    let user = m.mentionedJid && m.mentionedJid[0]
-    await conn.groupParticipantsUpdate(m.chat, [user], 'demote')
-
-    m.reply('Succes')
-
+    if (!isBotAdmin) return m.reply('I am not a admin, please promote me first')
+    if (!isAdmin) return m.reply('You are not an admin')
+    
+    let users = m.mentionedJid.filter(u => !areJidsSameUser(u, conn.user.id) && participants.includes(u))
+    if (users.includes(conn.user.id)) return m.reply('I can\'t demote myself')
+    if (users.length == 0) return m.reply('Mention someone to demote')
+    
+    try {
+        await conn.groupParticipantsUpdate(m.chat, users, 'demote')
+        m.reply('Success')
+    } catch (e) {
+        m.reply('Error: ' + e)
+    }
 }
+
 handler.help = ['demote @tag']
 handler.tags = ['group']
 handler.command = /^(demote)$/i
-
-handler.admin = true
-handler.group = true
-handler.botAdmin = true
 
 export default handler
