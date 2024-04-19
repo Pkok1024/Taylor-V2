6fa18d1a813acf39d7239563068888bcf2386e89
M.U.F.A.R <132034817+Pkok1024@users.noreply.github.com>
2024-04-19 12:29:41 +0700

    improve

--- plugins/Owner/owner-whitelist.js
+++ plugins/Owner/owner-whitelist.js
@@ -1,3 +1,5 @@
+const maxUsers = 50;
+
 let handler = async (m, {
     usedPrefix,
     command,
@@ -5,24 +7,7 @@ let handler = async (m, {
     args
 }) => {
     if (!args || !['add', 'remove'].includes(args[0].toLowerCase())) throw `
-*Penggunaan:* ${usedPrefix + command} <add|remove> nomor,nomor,...,nomor
-*Contoh:*
+*Usage:* ${usedPrefix + command} <add|remove> nomor,nomor,...,nomor
+*Example:*
 ${usedPrefix + command} add 6281111111111,12345678901,0
-${usedPrefix + command} remove 6281111111111,12345678901,0
-`.trim()
-    let type = args[0].toLowerCase() === 'add' ? true : false
-    let teks = text.replace(args[0], '').trim()
-    let users = teks.split(',').map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net')
-    for (let who of users) {
-        let user = global.db.data.users[who]
-        if (!user) user = global.db.data.users[who] = {}
-        user.whitelist = type
-    }
-    m.reply(`Berhasil ${type ? 'menambah' : 'menghapus'} whitelist ${users.length} pengguna`)
-}
-handler.help = ['whitelist'].map(v => v + ' nomor,nomor')
-handler.tags = ['owner']
-handler.command = ['whitelist']
-handler.owner = true
-
-export default handler
+${usedPrefix + command} remove 62811111111
