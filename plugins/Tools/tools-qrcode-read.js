d15b349a0e1fd0a4638aed677c4add4ede9820a0
M.U.F.A.R <132034817+Pkok1024@users.noreply.github.com>
2024-04-19 12:30:26 +0700

    comment

--- plugins/Tools/tools-qrcode-read.js
+++ plugins/Tools/tools-qrcode-read.js
@@ -1,33 +1,37 @@
-import { default as Jimp } from 'jimp';
-import { default as qrcode } from 'qrcode-reader';
+import { default as Jimp } from 'jimp'; // Importing Jimp library to manipulate images
+import { default as qrcode } from 'qrcode-reader'; // Importing qrcode-reader library to read QR codes
 
 let handler = async (m, { conn, usedPrefix, args, command }) => {
+    // Check if the quoted message contains an image with JPEG or PNG format
     const q = m.quoted ? m.quoted : m;
     const mime = (q.msg || q).mimetype || '';
-    if (!/image\/(jpe?g|png)/.test(mime)) return conn.reply(m.chat, 'Mohon kirim gambar dengan format JPEG atau PNG.', m);
-    const mediaData = await q.download();
-    const image = await Jimp.read(mediaData);
+    if (!/image\/(jpe?g|png)/.test(mime))
+        return conn.reply(m.chat, 'Mohon kirim gambar dengan format JPEG atau PNG.', m); // Reply with an error message if the image format is not supported
+
+    const mediaData = await q.download(); // Download the media data
+    const image = await Jimp.read(mediaData); // Read the image using Jimp
+
     try {
+        // Attempt to read the QR code from the image
         const result = await readQRCode(image);
-        await conn.reply(m.chat, result, m);
+        await conn.reply(m.chat, result, m); // Reply with the QR code value if successful
     } catch (error) {
-        await conn.reply(m.chat, 'Terjadi kesalahan dalam membaca QR code', m);
+        await conn.reply(m.chat, 'Terjadi kesalahan dalam membaca QR code', m); // Reply with an error message if there's an error in reading the QR code
     }
 };
 
+// Define the help, tags, and command for the handler
 handler.help = ['readqr'];
 handler.tags = ['tools'];
 handler.command = /^readqr(code)?$/i;
 
+// Export the handler
 export default handler;
 
+// Define the readQRCode function to read the QR code from an image
 const readQRCode = (image) => {
     return new Promise((resolve, reject) => {
+        // Create a new instance of the qrcode-reader
         const qr = new qrcode();
-        qr.callback = (err, value) => {
-            if (err) reject(err);
-            else resolve(value.result);
-        };
-        qr.decode(image.bitmap);
-    });
-};
+
+        // Define the callback function to handle the result or
