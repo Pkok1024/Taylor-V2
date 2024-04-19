117a03d96f89acd7d0b8e35626d8ac258b94ad65
M.U.F.A.R <132034817+Pkok1024@users.noreply.github.com>
2024-04-19 12:28:58 +0700

    improve

--- plugins/Others/joke.js
+++ plugins/Others/joke.js
@@ -1,22 +1,37 @@
-// Tolong jangan bully saya bang,, saya cuman mau bisnis :)
+// I am here to help you, kindly refrain from bullying
 import fetch from 'node-fetch'
 import fs from 'fs'
+import { Mime } from 'type-fest'
 
-let handler = async (m, {
-    conn,
-    command,
-    usedPrefix,
-    args
-}) => {
-    let pn = await fetch(`https://api.zacros.my.id/other/meme`)
-    let pnn = await pn.json()
-    let pnnnn = `*Result:* ${pnn.title}
-        Url: ${pnn.url}
-        Ups: ${pnn.ups}
-        Comment: ${pnn.comments}
-        `
-    await conn.sendFile(m.chat, pnn.image, '', pnnnn, m)
+const handler = async (m: any, { conn, command, usedPrefix, args }) => {
+  try {
+    const response = await fetch('https://api.zacros.my.id/other/meme')
+    const data = await response.json()
+
+    const imageUrl = data.image
+    const imageName = imageUrl.split('/').pop()
+    const imageType = imageUrl.split('.').pop() as Mime
+
+    const imageResponse = await fetch(imageUrl)
+    const imageBuffer = await imageResponse.buffer()
+
+    await fs.promises.writeFile(imageName, imageBuffer, 'binary')
+    await conn.sendMessage(m.chat, {
+      image: { url: `./${imageName}` },
+      caption: `*Result:* ${data.title}
+Url: ${data.url}
+Ups: ${data.ups}
+Comment: ${data.comments}
+`,
+    })
+
+    await fs.promises.unlink(imageName)
+  } catch (error) {
+    console.error(error)
+    await conn.sendMessage(m.chat, { text: 'An error occurred while fetching the meme. Please try again later.' })
+  }
 }
+
 handler.help = ['jokes (reply)']
 handler.tags = ['sticker']
 handler.command = /^jokes$/i
