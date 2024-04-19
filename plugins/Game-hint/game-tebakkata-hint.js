60301a0461985076ab6615aa623015fe476e1144
M.U.F.A.R <132034817+Pkok1024@users.noreply.github.com>
2024-04-19 12:28:58 +0700

    improve

--- plugins/Game-hint/game-tebakkata-hint.js
+++ plugins/Game-hint/game-tebakkata-hint.js
@@ -1,14 +1,17 @@
-let handler = async (m, {
-    conn
-}) => {
-    conn.tebakkata = conn.tebakkata ? conn.tebakkata : {}
-    let id = m.chat
-    if (!(id in conn.tebakkata)) throw false
-    let json = conn.tebakkata[id][1]
-    conn.reply(m.chat, '```' + json.jawaban.replace(/[AIUEOaiueo]/ig, '_') + '```', m)
+let handler = async (m, { conn }) => {
+  conn.tebakkata = conn.tebakkata || {};
+  let id = m.chat;
+  if (typeof id !== 'string') throw new Error('Invalid chat ID');
+  if (!(id in conn.tebakkata)) throw new Error('Game not found');
+  let json = conn.tebakkata[id];
+  if (!Array.isArray(json)) throw new Error('Invalid game data');
+  json = json[1];
+  if (typeof json.jawaban !== 'string') throw new Error('Invalid answer data');
+  let answer = json.jawaban.replace(/[AIUEOaiueo]/ig, '_');
+  if (answer.length === 0) throw new Error('Empty answer');
+  await conn.reply(m.chat, '```' + answer + '```', m);
 }
-handler.command = /^hkat$/i
+handler.command = /^hkat$/i;
+handler.limit = true;
 
-handler.limit = true
-
-export default handler
+module.exports = handler;
