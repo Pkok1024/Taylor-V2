583e92fb3b1a04de5bd3de71fefca003001fb5b2
M.U.F.A.R <132034817+Pkok1024@users.noreply.github.com>
2024-04-19 12:30:51 +0700

    comment

--- plugins/Others/edukasi-brainly.js
+++ plugins/Others/edukasi-brainly.js
@@ -1,18 +1,29 @@
-import * as baileys from "@whiskeysockets/baileys"
+import * as baileys from "@whiskeysockets/baileys" // Importing baileys library
 import {
     Brainly
-} from "brainly-scraper-v2"
-import fetch from "node-fetch"
+} from "brainly-scraper-v2" // Importing Brainly class from brainly-scraper-v2 library
+import fetch from "node-fetch" // Importing fetch function from node-fetch library
+
+// Initialize Brainly class with the user's ID
 Brainly.initialize();
 const brainly = new Brainly("id");
-let handler = async (m, {
+
+// Define the handler function for the brainly command
+const handler = async (m, {
     conn,
     text
 }) => {
+    // Throw an error if the user doesn't input a query
     if (!text) throw "Input Query"
+
+    // Reply to the user with a loading message
     await m.reply(wait)
+
     try {
+        // Search for the user's query on Brainly
         let res = await brainly.search(text, "id")
+
+        // Extract the question and answers from the search results
         let answer = res.map(({
             question,
             answers
@@ -21,13 +32,8 @@ let handler = async (m, {
 *Jawaban Ke ${i + 1}*${v.verification ? ' (Verified)' : ''}${v.isBest ? ' (Best)' : ''}
 ${v.content.replace(/(<br \/>)/gi, '\n')
           .replace(/(<([^>]+)>)/gi, '')}${v.attachments.length > 0 ? `\n*Media Url*: ${v.attachments.join(', ')}` : ''}`).join``}`).join('\n' + '-'.repeat(45))
+
+        // Reply to the user with the search results
         await m.reply(answer.trim())
     } catch (e) {
-        await m.reply(eror)
-    }
-}
-handler.help = handler.alias = ["brainly"]
-handler.tags = ["tools"]
-handler.command = /^(brainly)$/i
-
-export default handler
+        // If there's an error,
