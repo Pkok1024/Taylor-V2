65065c8ea49889bc1d2cb4599f1d2a47268b746b
M.U.F.A.R <132034817+Pkok1024@users.noreply.github.com>
2024-04-19 12:30:09 +0700

    improve

--- plugins/Islamic/islami-niatsholat.js
+++ plugins/Islamic/islami-niatsholat.js
@@ -1,18 +1,21 @@
 import fetch from 'node-fetch'
-let handler = async (m, {
-    usedPrefix,
-    command
-}) => {
-    try {
-        let res = await fetch('https://raw.githubusercontent.com/hamidamaulana/bacaan-sholat-main/main/assets/data/niatshalat.json')
-        let json = await res.json()
-        let niat = json.map((v, i) => `${i + 1}. ${v.name}\n↳ ${v.arabic}\n↳ ${v.latin}\n↳ ${v.terjemahan}`).join('\n\n')
-        m.reply(niat)
-    } catch (e) {
-        throw `_*Error!*_`
-    }
+
+const handler = async (m, { usedPrefix, command }) => {
+  try {
+    const res = await fetch('https://raw.githubusercontent.com/hamidamaulana/bacaan-sholat-main/main/assets/data/niatshalat.json')
+    const json = await res.json()
+    let niat = 'Niat Shalat:\n\n'
+    json.forEach((v, i) => {
+      niat += `${i + 1}. ${v.name}\n↳ ${v.arabic}\n↳ ${v.latin}\n↳ ${v.terjemahan}\n\n`
+    })
+    m.reply(niat)
+  } catch (e) {
+    m.reply(`_*Error!*_ ${e.message}`)
+  }
 }
+
 handler.help = ['niatshalat']
 handler.tags = ['islam']
 handler.command = /^(niatsh?(a|o)lat)$/i
+
 export default handler
