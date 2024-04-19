042e76d7d98f20aef96d1a0370cc02367f97df00
M.U.F.A.R <132034817+Pkok1024@users.noreply.github.com>
2024-04-19 12:28:59 +0700

    improve

--- plugins/Genshin/gi-outfit.js
+++ plugins/Genshin/gi-outfit.js
@@ -1,44 +1,20 @@
 import genshindb from 'genshin-db'
 
-let handler = async (m, {
-    conn,
-    text,
-    args,
-    usedPrefix,
-    command
-}) => {
-    if (!text) throw `Example : *${usedPrefix + command} outrider*`
+const handler = async (m, { conn, text, usedPrefix, limit }) => {
+  if (!text) throw `Example : ${usedPrefix}gioutfit outrider`
+
+  const checkProperty = (obj, props) => props.every(prop => obj.hasOwnProperty(prop))
+
+  try {
+    const anu = await genshindb.outfits(text)
+    if (!checkProperty(anu, ['name', 'description', 'character', 'url'])) throw new Error()
+
+    const ini_txt = `*Found : ${anu.name}*\n\n_"${anu.description}"_\n\n*Character :* ${anu.character}`
+    ini_txt += anu.url.modelviewer ? `\n_${anu.url.modelviewer}_` : ''
+    m.reply(ini_txt)
+  } catch (e) {
+    console.log(e)
     try {
-        let anu = await genshindb.outfits(text)
-        let ini_txt = `*Found : ${anu.name}*\n\n`
-        ini_txt += `_"${anu.description}"_\n\n`
-        ini_txt += `*Character :* ${anu.character}`
-        ini_txt += `${anu.url.modelviewer ? `\n_${anu.url.modelviewer}_` : ''}`
-        m.reply(ini_txt)
-    } catch (e) {
-        console.log(e)
-        try {
-            let anu2 = await genshindb.outfits(`${text}`, {
-                matchCategories: true
-            })
-            m.reply(`*List ${text} outfit :*\n\n- ` + anu2.toString().replaceAll(',', '\n- '))
-        } catch (e) {
-            console.log(e)
-            let anu2 = await genshindb.outfits(`names`, {
-                matchCategories: true
-            })
-            m.reply(`*Not Found*\n\n*Available outfits is :*\n${anu2.join(", ")}`)
-        }
-    }
-}
-
-handler.help = ['gioutfit <item>']
-handler.tags = ['genshin']
-handler.command = /^((gi|genshin)(costumes?|outfits?))$/i
-
-handler.limit = true
-
-export default handler
-
-const more = String.fromCharCode(8206)
-const readMore = more.repeat(4001)
+      const anu2 = await genshindb.outfits(text, { matchCategories: true })
+      if (Array.isArray(anu2) && anu2.length > 0) {
+        m.reply(`*List ${text} outfit :*\n\n- ${anu2.toString().replaceAll
