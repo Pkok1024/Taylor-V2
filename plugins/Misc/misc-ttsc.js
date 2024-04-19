24a3e94ef4761e4eb4265a6dd44da18c388caa00
M.U.F.A.R <132034817+Pkok1024@users.noreply.github.com>
2024-04-19 12:29:11 +0700

    improve

--- plugins/Misc/misc-ttsc.js
+++ plugins/Misc/misc-ttsc.js
@@ -1,192 +1,123 @@
-import {
-    randomUUID
-} from "crypto"
-let fetchear;
-import("node-fetch").then(function({
-    default: fetch
-}) {
-    fetchear = fetch
-})
-const fakeYouToken = "187b56b2217ac09dbe6ae610f19b35dfbc53cdd5857f818f03b45d048287b4bc"
+import { randomUUID } from "crypto";
+import fetch from "node-fetch";
 
-let handler = async (m, {
-    conn,
-    isOwner,
-    usedPrefix,
-    command,
-    args
-}) => {
-    let ListVoice = await (await fetch("https://api.fakeyou.com/tts/list")).json()
-    let lister = ListVoice.models
-    let readMore = String.fromCharCode(8206).repeat(4001);
+const fakeYouToken = "187b56b2217ac09dbe6ae610f19b35dfbc53cdd5857f818f03b45d048287b4bc";
 
-    let query = `Input query!\n\n*Example:*\n${usedPrefix + command} [angka]|[teks]\n\n*Pilih angka yg ada*\n` + readMore + lister.map((item, index) => "  " + (index + 1) + ". " + item.title).join("\n");
-    let text
-    if (args.length >= 1) {
-        text = args.slice(0).join(" ")
-    } else if (m.quoted && m.quoted.text) {
-        text = m.quoted.text
-    } else throw query
-    let [atas, bawah] = text.split("|")
-    if (!atas) return m.reply(query)
-    if (!bawah) return m.reply(query)
-    const {
-        modelToken,
-        title
-    } = await getModelByIndex(ListVoice, atas);
-    await m.reply(wait + "\n" + title)
+const fetchPatiently = async (url, params) => {
+  let response = await fetch(url, params);
+  while (response.status === 408 || response.status === 502) {
+    await new Promise((res) => setTimeout(res, 3000));
+    response = await fetch(url, params);
+  }
+  return response;
+};
 
-    try {
-        let res = await requestSpeech(modelToken, bawah)
-        if (res) await conn.sendFile(m.chat, res, '', '', m, null, {
-            ptt: true,
-            waveform: [100, 0, 100, 0, 100, 0, 100],
-            contextInfo: adReplyS.contextInfo
-        });
-    } catch (e) {
-        await m.reply(eror)
-    }
+const poll = async (token) => {
+  const response = await fetchPatiently(`https://api.fakeyou.com/tts/job/${token}`, {
+    method: "GET",
+    headers: {
+      Authorization: fakeYouToken,
+      Accept: "application/json",
+    },
+  });
 
-}
-handler.help = ["ttsc *number|your text*"]
-handler.tags = ["misc"]
-handler.command = /^(ttsc)$/i
-export default handler
+  if (!response.ok) {
+    throw new Error("Failed polling");
+  }
 
-async function getModelByIndex(arrayObject, index) {
-    const response = arrayObject;
+  const json = await response.json();
 
-    const model = response.models[index - 1];
-    if (model) {
-        const {
-            model_token,
-            title
-        } = model;
-        return {
-            modelToken: model_token,
-            title
-        };
-    } else {
-        throw new Error('Invalid index');
-    }
-}
+  if (json.state.status === "complete_success") {
+    return `https://storage.googleapis.com/vocodes-public${json.state.maybe_public_bucket_wav_audio_path}`;
+  }
 
-/*
-  Name: fetchPatiently(String url, Object params): Object
-  Description: Wrapper for node-fetch which retries upon 408 and 502 error codes
-  Returns: HTTP response
-*/
-async function fetchPatiently(url, params) {
-    let response = await fetchear(url, params);
-    while (response.status === 408 || response.status === 502) {
-        // Wait three seconds between each new request
-        await new Promise(res => setTimeout(res, 3000));
-        response = await fetchear(url, params);
-    }
-    return response;
-}
+  if (json.state.status === "pending" || json.state.status === "started" || json.state.status === "attempt_failed") {
+    return await poll(token);
+  }
 
-/*
-  Name: poll(String token): String
-  Description: Polls until a speech request is complete
-  Returns: URL on success, error string on failure
-*/
-function poll(token) {
-    return new Promise(async (resolve, reject) => {
+  throw new Error("Failed polling");
+};
 
-        // Wait one second between each poll request
-        await new Promise(res => setTimeout(res, 1000));
+const requestSpeech = async (voice, message) => {
+  const response = await fetchPatiently("https://api.fakeyou.com/tts/inference", {
+    method: "POST",
+    body: JSON.stringify({
+      tts_model_token: voice,
+      uuid_idempotency_token: randomUUID(),
+      inference_text: message,
+    }),
+    headers: {
+      Authorization: fakeYouToken,
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
 
-        // Retrieve status of current speech request
-        const response = await fetchPatiently("https://api.fakeyou.com/tts/job/" + token, {
-            method: "GET",
-            headers: {
-                "Authorization": fakeYouToken,
-                "Accept": "application/json"
-            }
-        }).catch(error => {
-            reject(`HTTP error! ${error.name}`);
-            console.error(error);
-        });
-        if (!response.ok) return;
+  if (!response.ok) {
+    throw new Error("Failed voice request");
+  }
 
-        const json = await response.json().catch(error => {
-            reject("Failed to parse poll JSON!");
-            console.error(error);
-        });
-        if (!json) return;
+  const json = await response.json();
 
-        if (!json.success) {
-            reject(`Failed polling! ${json.error_reason}`);
-            console.error(json);
-            return;
-        }
+  if (json.success) {
+    return await poll(json.inference_job_token);
+  }
 
-        switch (json.state.status) {
-            case "pending":
-            case "started":
-            case "attempt_failed": {
-                // Continue polling until success
-                await poll(token).then(resolve).catch(reject);
-                return;
-            }
-            case "complete_success": {
-                // Success, return audio URL
-                resolve("https://storage.googleapis.com/vocodes-public" + json.state.maybe_public_bucket_wav_audio_path);
-                return;
-            }
-            case "complete_failure":
-            case "dead":
-            default: {
-                // Failure, stop polling
-                reject(`Failed polling! ${json.state.status}`);
-                console.error(json);
-                return;
-            }
-        }
+  throw new Error("Failed voice request");
+};
+
+const handler = async (m, { conn, isOwner, usedPrefix, command, args }) => {
+  const ListVoice = await (await fetch("https://api.fakeyou.com/tts/list")).json();
+  const lister = ListVoice.models;
+  const readMore = String.fromCharCode(8206).repeat(4001);
+
+  const query = `Input query!\n\n*Example:*\n${usedPrefix + command} [angka]|[teks]\n\n*Pilih angka yg ada*\n` + readMore + lister.map((item, index) => "  " + (index + 1) + ". " + item.title).join("\n");
+  let text;
+  if (args.length >= 1) {
+    text = args.slice(0).join(" ");
+  } else if (m.quoted && m.quoted.text) {
+    text = m.quoted.text;
+  } else {
+    throw query;
+  }
+
+  const [atas, bawah] = text.split("|");
+  if (!atas || !bawah) {
+    throw query;
+  }
+
+  const { modelToken, title } = await getModelByIndex(ListVoice, atas);
+
+  await m.reply(wait + "\n" + title);
+
+  try {
+    const res = await requestSpeech(modelToken, bawah);
+    if (res) await conn.sendFile(m.chat, res, '', '', m, null, {
+      ptt: true,
+      waveform: [100, 0, 100, 0, 100, 0, 100],
+      contextInfo: adReplyS.contextInfo
     });
-}
+  } catch (e) {
+    await m.reply(eror);
+  }
+};
 
-/*
-  Name: requestSpeech(String voice, String message): String
-  Description: Requests speech and polls until job is complete
-  Returns: URL on success, error string on failure
-*/
-async function requestSpeech(voice, message) {
-    return new Promise(async (resolve, reject) => {
+handler.help = ["ttsc *number|your text*"];
+handler.tags = ["misc"];
+handler.command = /^(ttsc)$/i;
 
-        // Request generation of speech
-        const response = await fetchPatiently("https://api.fakeyou.com/tts/inference", {
-            method: "POST",
-            body: JSON.stringify({
-                tts_model_token: voice,
-                uuid_idempotency_token: randomUUID(),
-                inference_text: message
-            }),
-            headers: {
-                "Authorization": fakeYouToken,
-                "Accept": "application/json",
-                "Content-Type": "application/json"
-            }
-        }).catch(error => {
-            reject(`HTTP error! ${error.name}`);
-            console.error(error);
-        });
-        if (!response.ok) return;
+export default handler;
 
-        const json = await response.json().catch(error => {
-            reject("Failed to parse request JSON!");
-            console.error(error);
-        });
-        if (!json) return;
-
-        if (!json.success) {
-            reject(`Failed voice request! ${json.error_reason}`);
-            console.error(json);
-            return;
-        }
-
-        // Poll until request has been fulfilled
-        await poll(json.inference_job_token).then(resolve).catch(reject);
-    });
+const getModelByIndex = (arrayObject, index) => {
+  const response = arrayObject;
+  const model = response.models[index - 1];
+  if (model) {
+    const { model_token, title } = model;
+    return {
+      modelToken: model_token,
+      title
+    };
+  } else {
+    throw new Error('Invalid index');
+  }
 };
