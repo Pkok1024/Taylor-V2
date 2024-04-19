c434d678cbbfb623ff8d1d47e7fbe4f5f5269cad
M.U.F.A.R <132034817+Pkok1024@users.noreply.github.com>
2024-04-19 12:30:15 +0700

    improve

--- py/Infoga/recon/pgp.py
+++ py/Infoga/recon/pgp.py
@@ -1,34 +1,36 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*- 
-#
-# @name   : Infoga - Email Information Gathering
-# @url    : http://github.com/m4ll0k
-# @author : Momo Outaadi (m4ll0k)
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-from lib.output import *
-from lib.request import *
-from lib.parser import *
+"""
+@name: Infoga - Email Information Gathering
+@url: https://github.com/m4ll0k/Infoga
+@author: Momo Outaadi (m4ll0k)
+"""
+
+from lib.output import print_info
+from lib.request import Request
+from lib.parser import Parser
 
 class PGP(Request):
-	def __init__(self,target):
-		Request.__init__(self)
-		self.target = target
+    def __init__(self, target):
+        super().__init__()
+        self.target = target
 
-	def search(self):
-		test('Searching "%s" in PGP...'%(self.target))
-		url = "http://pgp.mit.edu/pks/lookup?search={target}&op=index".format(
-			target=self.target)
-		try:
-			resp = self.send(
-				method = 'GET',
-				url = url,
-				headers = {
-							'Host':'pgp.mit.edu'
-				}
-				)
-			return self.getemail(resp.content,self.target)
-		except Exception as e:
-			pass
+    def search(self):
+        print_info(f"Searching '{self.target}' in PGP...")
+        url = f"http://pgp.mit.edu/pks/lookup?search={self.target}&op=index"
+        try:
+            response = self.send(
+                method="GET",
+                url=url,
+                headers={
+                    "Host": "pgp.mit.edu"
+                }
+            )
+            return self.get_email(response.content, self.target)
+        except Exception as e:
+            print_info(f"Error: {e}", False)
 
-	def getemail(self,content,target):
-		return parser(content,target).email()
+    def get_email(self, content, target):
+        parser = Parser()
+        return parser.email(content, target)
