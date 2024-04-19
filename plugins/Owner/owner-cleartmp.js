e43ef7a86762ed3bc08e3523d7dce95eea38415c
M.U.F.A.R <132034817+Pkok1024@users.noreply.github.com>
2024-04-19 12:30:25 +0700

    comment

--- plugins/Owner/owner-cleartmp.js
+++ plugins/Owner/owner-cleartmp.js
@@ -1,20 +1,36 @@
+// Import required modules from 'fs' module
 import {
-    readdirSync,
-    rmSync
+    readdirSync, // Function to read directory
+    rmSync // Function to remove synchronously
 } from 'fs'
 
-let handler = async (m, {
+/*
+Define the handler function with the following parameters:
+- m: The message object
+- {conn, text}: An object containing the connection instance and the text of the command
+*/
+const handler = async (m, {
     conn,
     text
 }) => {
+    // Define the directory to be cleaned
     const dir = './tmp'
+
+    // Read the directory and synchronously remove each file
     readdirSync(dir).forEach(f => rmSync(`${dir}/${f}`));
+
+    // Prepare the response message
     let pesan = "The *tmp* folder has been cleaned"
+
+    // Reply to the message with the prepared message
     await m.reply(pesan)
 }
+
+// Add metadata to the handler function
 handler.help = ['cleartmp']
 handler.tags = ['owner']
 handler.owner = false
 handler.command = /^(cleartmp)$/i
 
+// Export the handler function as the default export
 export default handler
