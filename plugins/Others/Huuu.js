2dfeace97aa8fb017ad7de995edb73a6a7add24c
M.U.F.A.R <132034817+Pkok1024@users.noreply.github.com>
2024-04-19 12:30:04 +0700

    comment

--- plugins/Others/Huuu.js
+++ plugins/Others/Huuu.js
@@ -1,46 +1,53 @@
 import {
-    sticker
+    sticker // Import the sticker function from '../../lib/sticker.js'
 } from '../../lib/sticker.js'
-import fetch from 'node-fetch'
+import fetch from 'node-fetch' // Import the fetch function from 'node-fetch'
 
-let handler = async (m, {
-    conn,
-    text,
-    usedPrefix,
-    command
+let handler = async (m, { // Define the handler function with async signature
+    conn, // Import the connection object
+    text, // Import the message text
+    usedPrefix, // Import the prefix used in the message
+    command // Import the command used in the message
 }) => {
-    let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-    let pp = await conn.profilePictureUrl(who).catch(_ => hwaifu.getRandom())
-    let name = await conn.getName(who)
+    let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender // Get the ID of the mentioned user or the user who sent the message
+    let pp = await conn.profilePictureUrl(who).catch(_ => hwaifu.getRandom()) // Get the profile picture URL of the user
+    let name = await conn.getName(who) // Get the name of the user
 
+    // Fetch a random sticker from one of the URLs in the stikerhuuu array
     let stiker = await sticker(null, global.API(`${pickRandom(stikerhuuu)}`), global.packname, global.author)
-    if (stiker) return await conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, null, {
-        fileLength: fsizedoc,
-        contextInfo: {
-            externalAdReply: {
-                showAdAttribution: true,
-                mediaUrl: sig,
-                mediaType: 2,
-                description: wm,
-                title: '👋 Hai, ' + name + ' ' + ucapan,
-                body: botdate,
-                thumbnail: await (await fetch(pp)).arrayBuffer(),
-                sourceUrl: sgc
+    if (stiker) return await conn.sendFile(m.chat, stiker, 'stiker.webp', '', m, null, { // Send the sticker as a file
+        fileLength: fsizedoc, // Include the file size in the metadata
+        contextInfo: { // Include context information in the metadata
+            externalAdReply: { // Include an external ad reply in the metadata
+                showAdAttribution: true, // Show the ad attribution
+                mediaUrl: sig, // Include the media URL
+                mediaType: 2, // Set the media type to image
+                description: wm, // Include a description
+                title: '👋 Hai, ' + name + ' ' + ucapan, // Set the title of the ad reply
+                body: botdate, // Include the bot date in the ad reply
+                thumbnail: await (await fetch(pp)).arrayBuffer(), // Set the thumbnail of the ad reply to the user's profile picture
+                sourceUrl: sgc // Include the source URL
             }
         }
     })
-    throw stiker.toString()
+    throw stiker.toString() // Throw an error with the sticker object as a string
 
 }
 
+// Set the custom prefix of the handler to /^(huuu)$/i
 handler.customPrefix = /^(huuu)$/i
+
+// Set the command of the handler to a regular expression that matches the command
 handler.command = new RegExp
 
-export default handler
+export default handler // Export the handler as the default export
 
+// Define a function to pick a random element from an array
 function pickRandom(list) {
-    return list[Math.floor(list.length * Math.random())]
+    return list[Math.floor(list.length * Math.random())] // Pick a random element from the array and return it
 }
+
+// Define an array of sticker URLs
 let stikerhuuu = [
     "https://telegra.ph/file/fa2bbea0f7de2575cf027.png", //patrick huu
     "https://telegra.ph/file/4a2db7bc9f3f9ecfc007d.png", //anime yntkts
@@ -49,16 +56,4 @@ let stikerhuuu = [
     "https://telegra.ph/file/2ade25087c89f86587853.png", //pak polisi pap tt
     "https://telegra.ph/file/eb2b5e5fff569896c1639.png", //kucing1
     "https://telegra.ph/file/bd8a0e7ea01218531798b.png", //kacamata
-    "https://telegra.ph/file/300610838ffa0e6576eb9.png", //patrick pembohong
-    "https://telegra.ph/file/954afe562e58c144620ae.png", //spongebob FBI
-    "https://telegra.ph/file/72026dcc46e4cb4b6f9ae.png", //mazowski monster inc
-    "https://telegra.ph/file/aa9f1bea869e362e6f56e.png", //wkwk
-    "https://telegra.ph/file/09bbff0da316ba21b4f8e.png", //kucing2
-    "https://telegra.ph/file/2e0637d57e3cc1abcb4a0.png", //patrick anak setan
-    "https://telegra.ph/file/d771ae015b5486859d03f.png", //mazowski 2
-    "https://telegra.ph/file/9c7606f571c05b4d0c941.png", //hengker
-    "https://telegra.ph/file/84fd937257bcd614d6c9e.png", //anjing
-    "https://telegra.ph/file/b8ba6989c00c50df049d0.png", //bapak bapak lovee
-    "https://telegra.ph/file/2f618fffab6ff7bea32ab.png", //abang saleh
-    "https://telegra.ph/file/dfbf483c209a31f01b4e5.png" //hengker2
-]
+    "https://telegra.ph/file/300610838ffa0e6576eb9.png",
