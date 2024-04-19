c6ed4ea74d0919003495c3759cb23b0466dd111c
M.U.F.A.R <132034817+Pkok1024@users.noreply.github.com>
2024-04-19 12:29:32 +0700

    comment

--- plugins/Downloader/downloader-soundcloud.js
+++ plugins/Downloader/downloader-soundcloud.js
@@ -1,10 +1,17 @@
+// Import necessary modules
 const CLIENT_ID = 'zZeR6I5DM5NMAYEhk7J9vveMqZzpCIym';
-import soundcloud from 'soundcloud-downloader';
-import fetch from 'node-fetch';
-import util from 'util';
-import { getBuffer } from '../lib/myFunc.js';
+import soundcloud from 'soundcloud-downloader'; // Soundcloud downloader module
+import fetch from 'node-fetch'; // Node-fetch module for making HTTP requests
+import util from 'util'; // Util module for providing utility functions
+import { getBuffer } from '../lib/myFunc.js'; // Custom function for getting buffer
 
-const handler = async (m, {
-    conn,
-    args,
+// Define the async handler function for the bot
+const handler = async (m, { 
+  conn, // Connection object for interacting with the bot
+  args, // Arguments provided to the command
+}) => {
+  // Your code logic goes here
+}
 
+// Export the handler function to make it available for use
+module.exports = handler;
