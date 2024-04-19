66d1467b245df93b56b41a974e8a408dda18dddf
M.U.F.A.R <132034817+Pkok1024@users.noreply.github.com>
2024-04-19 12:30:53 +0700

    comment

--- plugins/Fun/fun-generate-nama.js
+++ plugins/Fun/fun-generate-nama.js
@@ -1,8 +1,13 @@
+// This is a function handler that will process messages containing a specific command
 function handler(m, {
-    text
+    text // The text of the message
 }) {
+    // Extract the text to be replaced from the message
     let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
+
+    // Replace each letter in the text with its corresponding Japanese word
     m.reply(teks.replace(/[a-z]/gi, v => {
+        // Define a replacement object for each letter
         return {
             'a': 'ka',
             'b': 'tu',
@@ -30,11 +35,18 @@ function handler(m, {
             'x': 'na',
             'y': 'fu',
             'z': 'zi'
-        } [v.toLowerCase()] || v
+        } [v.toLowerCase()] || v // If the letter is not found in the object, return the original letter
     }))
 }
+
+// Add a help command for the handler
 handler.help = ['namaninja'].map(v => v + ' <teks>')
+
+// Tag the handler as a fun command
 handler.tags = ['fun']
+
+// Define the command that triggers the handler
 handler.command = /^(namaninja|namae)$/i
 
+// Export the handler for use in other modules
 export default handler
