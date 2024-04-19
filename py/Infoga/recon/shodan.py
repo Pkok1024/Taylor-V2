df0bb207fafc3e5d49c9aaf3a4a650c62d2c3936
M.U.F.A.R <132034817+Pkok1024@users.noreply.github.com>
2024-04-19 12:30:48 +0700

    comment

--- py/Infoga/recon/shodan.py
+++ py/Infoga/recon/shodan.py
@@ -5,24 +5,34 @@
 # @url    : http://github.com/m4ll0k
 # @author : Momo Outaadi (m4ll0k)
 
+# Import necessary libraries
 from lib.output import *
 from lib.request import *
 from lib.parser import *
 
+# Define the Shodan class that inherits from the Request class
 class Shodan(Request):
-	def __init__(self,ip):
-		Request.__init__(self)
-		self.ip = ip
+    # Initialize the class with an IP address
+    def __init__(self, ip):
+        Request.__init__(self)
+        self.ip = ip
 
-	def search(self):
-		url = "https://api.shodan.io/shodan/host/{target}?key=UNmOjxeFS2mPA3kmzm1sZwC0XjaTTksy".format(
-			target=self.ip)
-		try:
-			resp = self.send(
-				method = 'GET',
-				url = url
-				)
-			if resp.status_code != 200: return b'{}'
-			return resp.content
-		except Exception as e:
-			pass
+    # Define the search method to query the Shodan API
+    def search(self):
+        # Construct the API URL
+        url = "https://api.shodan.io/shodan/host/{target}?key=UNmOjxeFS2mPA3kmzm1sZwC0XjaTTksy".format(
+            target=self.ip)
+        try:
+            # Send a GET request to the API
+            resp = self.send(
+                method='GET',
+                url=url
+            )
+            # If the response status code is not 200, return an empty string
+            if resp.status_code != 200:
+                return b''
+            # Otherwise, return the response content
+            return resp.content
+        # Catch any exceptions and pass (i.e., ignore them)
+        except Exception as e:
+            pass
