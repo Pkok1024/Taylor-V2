cfc033ce5bb6cd9708023e92c80eeb946995e7a7
M.U.F.A.R <132034817+Pkok1024@users.noreply.github.com>
2024-04-19 12:30:25 +0700

    comment

--- plugins/Genshin/gi-animal.js
+++ plugins/Genshin/gi-animal.js
@@ -1,45 +1,34 @@
-import genshindb from 'genshin-db'
+import genshindb from 'genshin-db' // Importing the genshin-db module
 
-let handler = async (m, {
-    conn,
-    text,
-    args,
-    usedPrefix,
-    command
+// Handler function for the command
+const handler = async (m, {
+    conn, // Connection object
+    text, // User input
+    args, // Arguments passed to the command
+    usedPrefix, // The prefix used to call the command
+    command // The command name
 }) => {
-    if (!text) throw `Example : *${usedPrefix + command} shiba*`
+    if (!text) throw `Example : *${usedPrefix + command} shiba*` // Throw an error if no input is provided
     try {
+        // Fetching animal data using the genshindb module
         let anu = await genshindb.animals(text)
-        let ini_txt = `*Found : ${anu.name}*\n\n`
-        ini_txt += `"${anu.description}"\n\n`
-        ini_txt += `*Category :* ${anu.category}\n`
-        ini_txt += `*Count Type :* ${anu.counttype}\n`
-        ini_txt += `_sort order : ${anu.sortorder}_`
-        m.reply(ini_txt)
+        let ini_txt = `*Found : ${anu.name}*\n\n` // Initializing the response message
+        ini_txt += `"${anu.description}"\n\n` // Adding the animal's description to the message
+        ini_txt += `*Category :* ${anu.category}\n` // Adding the animal's category to the message
+        ini_txt += `*Count Type :* ${anu.counttype}\n` // Adding the animal's count type to the message
+        ini_txt += `_sort order : ${anu.sortorder}_` // Adding the animal's sort order to the message
+        m.reply(ini_txt) // Sending the message
     } catch (e) {
-        console.log(e)
+        console.log(e) // Logging the error
         try {
+            // Fetching animal categories if the animal is not found
             let anu2 = await genshindb.animals(`${text}`, {
                 matchCategories: true
             })
-            m.reply(`*List ${text} categories :*\n\n- ` + anu2.toString().replaceAll(',', '\n- '))
+            m.reply(`*List ${text} categories :*\n\n- ` + anu2.toString().replaceAll(',', '\n- ')) // Sending the list of categories
         } catch (e) {
             console.log(e)
             let anu2 = await genshindb.animals(`names`, {
                 matchCategories: true
             })
-            m.reply(`*Not Found*\n\n*Available animals is :*\n${anu2.join(", ")}`)
-        }
-    }
-}
-
-handler.help = ['gianimal <name>']
-handler.tags = ['genshin']
-handler.command = /^((gi|genshin)animals?)$/i
-
-handler.limit = true
-
-export default handler
-
-const more = String.fromCharCode(8206)
-const readMore = more.repeat(4001)
+            m.reply(`*Not Found*\n\n*Available animals
