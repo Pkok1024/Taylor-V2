e884fc59a9b6cf02052316dd9a382e6dcd3773b8
M.U.F.A.R <132034817+Pkok1024@users.noreply.github.com>
2024-04-19 12:30:09 +0700

    improve

--- plugins/Misc/misc-bonk.js
+++ plugins/Misc/misc-bonk.js
@@ -1,23 +1,34 @@
-import jimp from 'jimp'
+import jimp from 'jimp';
+import { getProfilePicture } from '@adiwajshing/baileys';
 
-let handler = async (m, {
-    conn,
-    text
-}) => {
-    let img = await jimp.read('https://i.imgur.com/nav6WWX.png'),
-        who = m.mentionedJid?.[0] || m.quoted?.sender || m.sender,
-        avatar = await jimp.read(await conn.profilePictureUrl(who, 'image')),
-        bonk = await img.composite(avatar.resize(128, 128), 120, 90, {
-            mode: 'dstOver',
-            opacitySource: 1,
-            opacityDest: 1
-        }).getBufferAsync('image/png')
-    conn.sendMessage(m.chat, {
-        image: bonk
-    }, {
-        quoted: m
-    })
-}
-handler.command = /^(bonk)$/i
+const handler = async (m, { conn, args }) => {
+  try {
+    // Get the first mentioned user or the sender if not mentioned
+    const who = m.mentionedJid?.[0] || m.quoted?.sender || m.sender;
 
-export default handler
+    // Load the base image
+    const baseImage = await jimp.read('https://i.imgur.com/nav6WWX.png');
+
+    // Load the user's profile picture
+    const avatar = await getProfilePicture(conn, who);
+    const avatarImage = await jimp.read(avatar);
+    const resizedAvatar = avatarImage.resize(128, 128);
+
+    // Composite the avatar onto the base image
+    const compositedImage = await baseImage.composite(resizedAvatar, 120, 90, {
+      mode: jimp.BLEND_SOURCE_OVER,
+      opacitySource: 1,
+      opacityDest: 1,
+    });
+
+    // Send the resulting image
+    await conn.sendMessage(m.chat, { image: await compositedImage.getBufferAsync('image/png') }, { quoted: m });
+  } catch (err) {
+    console.error(err);
+    await conn.reply(m.chat, 'An error occurred while processing the image.', m);
+  }
+};
+
+handler.command = /^(bonk)$/i;
+
+export default handler;
