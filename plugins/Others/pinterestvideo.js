7025d69f9e16ba984939b4d1c00d74d5e7bcc617
M.U.F.A.R <132034817+Pkok1024@users.noreply.github.com>
2024-04-19 12:30:30 +0700

    comment

--- plugins/Others/pinterestvideo.js
+++ plugins/Others/pinterestvideo.js
@@ -1,6 +1,6 @@
 import {
     spin
-} from '../../lib/scraper/scrape.js'
+} from '../../lib/scraper/scrape.js' // Importing the 'spin' function from the scraper library
 
 let handler = async (m, {
     conn,
@@ -9,20 +9,32 @@ let handler = async (m, {
     command
 }) => {
 
+    // Check if the user provided a URL argument
     if (!args[0]) throw `*Perintah ini untuk mengunduh video dari pinterest dengan link*\n\ncontoh:\n${usedPrefix + command} https://id.pinterest.com/pin/27162403992537372/`
+
+    // Validate the URL argument to ensure it's a Pinterest link
     if (!args[0].match(/https:\/\/.*pinterest.com\/pin|pin.it/gi)) throw `*Link salah! Perintah ini untuk mengunduh video dari pinterest dengan link*\n\ncontoh:\n${usedPrefix + command} https://id.pinterest.com/pin/27162403992537372/`
+
+    // Call the 'spin' function with the provided URL argument
     await spin(args[0]).then(async res => {
+
+        // Parse the response from the 'spin' function as JSON
         let pin = JSON.stringify(res)
         let json = JSON.parse(pin)
+
+        // Check if the response indicates that the video could not be downloaded
         if (!json.status) throw `Tidak dapat diunduh`
+
+        // Send the downloaded video file to the user
         await conn.sendFile(m.chat, json.data.url, '', `*Mythia Batford*`, m)
     })
 
 }
+
+// Provide some help text for the user
 handler.help = ['pinterestvideo'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 handler.command = /^pinterestvideo$/i
 
-handler.limit = true
-
-export default handler
+// Set a limit for the number of times this function can be called
+handler.limit =
