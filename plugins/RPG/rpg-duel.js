3441d4a92f237a4d581e1b39493b68f435b821ae
M.U.F.A.R <132034817+Pkok1024@users.noreply.github.com>
2024-04-19 12:29:21 +0700

    improve

--- plugins/RPG/rpg-duel.js
+++ plugins/RPG/rpg-duel.js
@@ -1,91 +1,94 @@
+const {
+    clockString,
+    pickRandom
+} = require('./helpers')
+
 let handler = async (m, {
     conn,
     text,
     args,
     command
 }) => {
-    conn.duel = conn.duel ? conn.duel : []
-    args.length != 0 ? conn.duel.push(m.mentionedJid ? m.mentionedJid[0] : (args[0].replace(/[@ .+-]/g, '').replace(' ', '') + '@s.whatsapp.net')) : ""
-    let who = conn.duel[0]
-    //let kita = conn.duel[m.sender]
-    let enemy = global.db.data.users[who]
+    let duel = conn.duel || []
+    let opponent
+
+    if (args.length > 0) {
+        opponent = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : text.match(/\@(\d{0,16})/g)[0] ? text.match(/\@(\d{0,16})/g)[0].replace(/@/g, '') : ''
+        if (!opponent) return m.reply('Tag salah satu musuh yang ingin duel!')
+        if (duel.includes(opponent)) return m.reply('Musuh sudah ada dalam daftar duel!')
+        duel.push(opponent)
+    } else {
+        opponent = duel[0]
+    }
+
     let user = global.db.data.users[m.sender]
-    let count = args[1] && args[1].length > 0 ? Math.min(100, Math.max(parseInt(args[1]), 1)) : Math.min(1)
+    let enemy = global.db.data.users[opponent]
+
+    if (!opponent) return m.reply('Belum ada musuh yang ditag!')
+    if (!enemy) return m.reply('Musuh tidak ditemukan!')
+
+    let count = parseInt(args[1]) || 1
+    count = Math.min(100, Math.max(count, 1))
+
     let nama = await conn.getName(m.sender)
 
-    let randomaku = `${Math.floor(Math.random() * 101)}`.trim()
-    let randomkamu = `${Math.floor(Math.random() * 81)}`.trim()
-    let Aku = (randomaku * 1)
-    let Kamu = (randomkamu * 1)
+    let randomAku = Math.floor(Math.random() * 101)
+    let randomKamu = Math.floor(Math.random() * 81)
+    let Aku = randomAku
+    let Kamu = randomKamu
 
-    let __timers = (new Date - user.lastduel)
-    let _timers = (300000 - __timers)
-    let timers = clockString(_timers)
+    let lastDuel = new Date - user.lastduel
+    let timer = 300000 - lastDuel
+    timer = clockString(timer)
 
     try {
         if (/duel/.test(command)) {
-            if (!who) return m.reply('tag yg ingin di ajak duel!')
-
-            let pler = `@${m.sender.replace(/@.+/, '')} Mengajak duel ${args[0]}\n\nPilih Y Atau No`
+            if (!opponent) return m.reply('Tag salah satu musuh yang ingin duel!')
+            let teks = `@${m.sender.replace(/@.+/, '')} Mengajak duel ${opponent}\n\nPilih Y Atau No`
             let mentionedJid = [m.sender]
-
-            if (new Date - user.lastduel > 300000) {
-                conn.reply(m.chat, pler, m, {
+            if (lastDuel < 300000) {
+                return conn.reply(m.chat, `Kamu Sudah Berduel Tunggu hingga ${timer}`, m)
+            } else {
+                conn.reply(m.chat, teks, m, {
                     mentions: conn.parseMention(mentionedJid)
                 })
-
-            } else conn.reply(m.chat, `Kamu Sudah Berduel Tunggu hingga ${timers}`, m)
+            }
         }
 
-        if (/dya/.test(command)) {
-            let kenal = !who.includes(m.sender)
-            if (kenal) throw 'Lu siapa?\nkok ikut kut mau duel'
+        if (/dya|yes/.test(command)) {
+            if (!duel.includes(opponent)) return m.reply('Belum ada musuh yang ditag!')
             user.lastduel = new Date * 1
             if (Aku > Kamu) {
                 user.money -= 900
                 enemy.money += 900
-                delete conn.duel[m.sender]
-                conn.reply(m.chat, `@${who.split("@")[0]} Menang Gelud🤼\n*Hadiah:*\n900 Money buat beli gorengan`.trim(), m)
+                delete conn.duel[opponent]
+                conn.reply(m.chat, `@${opponent} Menang Gelud🤼\n*Hadiah:*\n900 Money buat beli gorengan`.trim(), m)
             } else if (Aku < Kamu) {
                 user.money += 450
                 enemy.money -= 450
-                delete conn.duel[m.sender]
-                conn.reply(m.chat, `@${who.split("@")[0]} Kalah Gelud🤼\n*Hadiah:*\n 450 money Mayan buat beli Limit`.trim(), m)
+                delete conn.duel[opponent]
+                conn.reply(m.chat, `@${opponent} Kalah Gelud🤼\n*Hadiah:*\n 450 money Mayan buat beli Limit`.trim(), m)
             } else {
                 user.money += 250
                 enemy.money += 250
-                delete conn.duel[m.sender]
-                conn.reply(m.chat, `@${who.split("@")[0]}\n *Seri*\n masing masing 250 Money`.trim(), m)
+                delete conn.duel[opponent]
+                conn.reply(m.chat, `@${opponent}\n *Seri*\n masing masing 250 Money`.trim(), m)
             }
         }
-        if (/dno/.test(command)) {
-            let kenal = !who.includes(m.sender)
+        if (/dno|no/.test(command)) {
+            let kenal = !duel.includes(m.sender)
             if (kenal) return conn.reply(m.chat, `Lu siapa?\nkok ikut kut mau duel`, m)
-            //if (!who) return m.reply('tag yg ingin di ajak duel!')
-            conn.reply(m.chat, `@${who.split("@")[0]} Membatalkan Ajakan Duel`, m)
-            delete conn.duel[m.sender]
+            conn.reply(m.chat, `@${opponent} Membatalkan Ajakan Duel`, m)
+            delete conn.duel[opponent]
         }
     } catch (e) {
-        //return conn.sendBut( m.chat, `Sepertinya ada bug`, `laporkan ke owner`, `Kanjut Badag`, `+bug duel ${e.stack}`, m)
         return m.reply(`${e}`)
     }
 }
 
 handler.help = ['duel @tag']
 handler.tags = ['rpg']
-handler.command = /^(duel|dya|dno)/i
+handler.command = /^(duel|yes|no|y|n)$/i
 handler.group = true
 
-export default handler
-
-function pickRandom(list) {
-    return list[Math.floor(Math.random() * list.length)]
-}
-
-function clockString(ms) {
-    let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000)
-    let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
-    let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-    let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-    return ['\n' + d, ' *Days ☀️*\n ', h, ' *Hours 🕐*\n ', m, ' *Minute ⏰*\n ', s, ' *Second ⏱️* '].map(v => v.toString().padStart(2, 0)).join('')
-}
+module.exports = handler
