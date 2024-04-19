b6490afff3438d79d5ae147442fa61d743671695
M.U.F.A.R <132034817+Pkok1024@users.noreply.github.com>
2024-04-19 12:30:31 +0700

    comment

--- plugins/Owner/owner-sf.js
+++ plugins/Owner/owner-sf.js
@@ -1,18 +1,39 @@
+// Importing the 'fs' module for file system operations
 import fs from 'fs'
-let handler = async (m, {
-    text,
-    usedPrefix,
-    command
-}) => {
-    if (!text) throw `uhm.. teksnya mana?\n\npenggunaan:\n${usedPrefix + command} <teks>\n\ncontoh:\n${usedPrefix + command} plugins/melcanz.js`
-    if (!m.quoted.text) throw `balas pesan nya!`
+
+// Defining the handler function for the 'sf' command
+const handler = async (m, { text, usedPrefix, command }) => {
+    // Check if the text argument is not provided
+    if (!text) {
+        // Throw an error message
+        throw `uhm.. teksnya mana?\n\npenggunaan:\n${usedPrefix + command} <teks>\n\ncontoh:\n${usedPrefix + command} plugins/melcanz.js`
+    }
+
+    // Check if the quoted message is not provided
+    if (!m.quoted.text) {
+        // Throw an error message
+        throw `balas pesan nya!`
+    }
+
+    // Define the path for the file to be saved
     let path = `${text}`
+
+    // Save the quoted message to the file
     await fs.writeFileSync(path, m.quoted.text)
+
+    // Reply to the user with the file path
     m.reply(`tersimpan di ${path}`)
 }
+
+// Set the help and tags for the command
 handler.help = ['sf'].map(v => v + ' <teks>')
 handler.tags = ['owner']
+
+// Set the command name and pattern
 handler.command = /^sf$/i
 
+// Set the command as owner-only
 handler.rowner = true
+
+// Export the handler function
 export default handler
