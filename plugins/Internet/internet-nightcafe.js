f5c681e5539959179ede3a33eb22582c0bf9943a
M.U.F.A.R <132034817+Pkok1024@users.noreply.github.com>
2024-04-19 12:30:19 +0700

    improve

--- plugins/Internet/internet-nightcafe.js
+++ plugins/Internet/internet-nightcafe.js
@@ -2,54 +2,49 @@ import axios from 'axios'
 import fetch from 'node-fetch'
 import cheerio from 'cheerio'
 
-let handler = async (m, {
+const handler = async (m, {
     conn,
     args,
     usedPrefix,
     text,
     command
 }) => {
-    let res = await nightcafe(text)
-    let listSections = []
-    Object.values(res).map((v, index) => {
-        listSections.push([index + ' ' + cmenub + ' ' + v.judul, [
-            ['Get Image', usedPrefix + 'get ' + v.thumb, '\n⌚ *By:* ' + author + '\n *Link:* ' + v.thumb]
-        ]])
-    })
-    return conn.sendList(m.chat, htki + ' 📺 nightcafe Search 🔎 ' + htka, `⚡ Silakan pilih nightcafe Search di tombol di bawah...\n*Teks yang anda kirim:* ${text}\n\nKetik ulang *${usedPrefix + command}* teks anda untuk mengubah teks lagi`, author, `☂️ nightcafe Search Disini ☂️`, listSections, m)
+    try {
+        const res = await nightcafe(text)
+        const listSections = Object.entries(res).map(([index, { judul, thumb }], i) => [
+            `${i + 1}. ${cmenub} ${judul.trim()}`,
+            [
+                ['Get Image', usedPrefix + 'get ' + thumb, `\n⌚ By: ${author}\nLink: ${thumb}`]
+            ]
+        ])
+        return conn.sendList(m.chat, htki + ' 📺 nightcafe Search 🔎 ' + htka, `⚡ Silakan pilih nightcafe Search di tombol di bawah...\n*Teks yang anda kirim:* ${text}\n\nKetik ulang *${usedPrefix + command}* teks anda untuk mengubah teks lagi`, author, `☂️ nightcafe Search Disini ☂️`, listSections, m)
+    } catch (err) {
+        return conn.reply(m.chat, `Error: ${err.message}`, m)
+    }
 }
 handler.help = ['nightcafe']
 handler.tags = ['internet']
 handler.command = /^nightcafe$/i
 
-export default handler
-
-/* New Line */
-async function nightcafe(query) {
-    return new Promise((resolve) => {
-        axios.get('https://creator.nightcafe.studio/explore?q=' + query)
-            .then(({
-                data
-            }) => {
-                const $ = cheerio.load(data)
-                const thumb = [];
-                const judul = [];
-                const result = [];
-                $('img').each(function(a, b) {
-                    if ($(b).attr('src').startsWith('https://images.nightcafe.studio/')) {
-                        thumb.push($(b).attr('src'))
-                    }
-                })
-                $('img').each(function(a, b) {
-                    judul.push($(b).attr('alt'))
-                })
-                for (let i = 0; i < thumb.length; i++) {
-                    result.push({
-                        thumb: thumb[i],
-                        judul: judul[i]
-                    })
-                }
-                resolve(result)
-            })
+const nightcafe = async (query) => {
+    const { data } = await axios.get(`https://creator.nightcafe.studio/explore?q=${query}`)
+    const $ = cheerio.load(data)
+    const thumb = []
+    const judul = []
+    $('img').each((i, el) => {
+        const src = $(el).attr('src')
+        if (src.startsWith('https://images.nightcafe.studio/')) {
+            thumb.push(src)
+        }
     })
+    $('img').each((i, el) => {
+        judul.push($(el).attr('alt'))
+    })
+    const result = thumb.reduce((acc, cur, i) => {
+        acc[i] = { judul: judul[i], thumb: cur }
+        return acc
+    }, {})
+    return result
 }
+
+export default handler
