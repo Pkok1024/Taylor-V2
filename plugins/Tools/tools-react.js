98b5055858a2e89c35b6117e8a767e2e805e1426
M.U.F.A.R <132034817+Pkok1024@users.noreply.github.com>
2024-04-19 12:30:34 +0700

    comment

--- plugins/Tools/tools-react.js
+++ plugins/Tools/tools-react.js
@@ -1,38 +1,44 @@
+// This function handles the 'react' command, which allows users to add a reaction emoji to a message.
 let handler = async (m, {
-    conn,
-    usedPrefix: _p,
-    args,
-    text,
-    usedPrefix
+    conn, // The Bot's connection object.
+    usedPrefix: _p, // The prefix used in the command.
+    args, // An array of arguments passed in the command.
+    text, // The text content of the message.
+    usedPrefix // The prefix used in the command.
 }) => {
     try {
+        // Check if the user has quoted a message to react to.
         if (!m.quoted) throw 'Balas Chatnya !';
+
+        // Check if only one emoji is provided.
         if (text.length > 2) throw 'Cuma Untuk 1 Emoji!';
+
+        // Check if an emoji has been provided.
         if (!text) throw `📍 Contoh Penggunaan :\n${usedPrefix}react 🗿`;
 
+        // Add the reaction to the quoted message.
         await conn.relayMessage(m.chat, {
             reactionMessage: {
                 key: {
-                    id: m.quoted.id,
-                    remoteJid: m.chat,
-                    fromMe: true
+                    id: m.quoted.id, // The ID of the message to react to.
+                    remoteJid: m.chat, // The ID of the chat where the message is located.
+                    fromMe: true // Whether the message is from the bot or not.
                 },
-                text: text
+                text: text // The emoji to add as a reaction.
             }
         }, {
-            messageId: m.id
+            messageId: m.id // The ID of the message being sent.
         });
     } catch (error) {
         try {
+            // If the above method fails, try to add the reaction using the sendReact method.
             await conn.sendReact(m.chat, text, m.quoted.vM.key);
         } catch (error) {
-            await conn.reply(m.chat, eror, m);
+            // If both methods fail, send an error message to the user.
+            await conn.reply(m.chat, error, m);
         }
     }
 };
 
+// The help message for the command.
 handler.help = ['react <emoji>'];
-handler.tags = ['Baileys'];
-handler.command = /^(reac(tions|(t(ion)?)?)|addreact)$/i;
-
-export default handler;
