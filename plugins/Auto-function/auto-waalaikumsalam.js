d68ab21577f3389dad19c0903b339fe90bee87fa
M.U.F.A.R <132034817+Pkok1024@users.noreply.github.com>
2024-04-19 12:28:42 +0700

    improve

--- plugins/Auto-function/auto-waalaikumsalam.js
+++ plugins/Auto-function/auto-waalaikumsalam.js
@@ -1,16 +1,20 @@
+const {
+    MessageType
+} = require('@adiwajshing/baileys')
+
 let handler = async (m, {
-    conn
+    conn,
+    text,
+    usedPrefix
 }) => {
-    let caption = `*Waalaikummussalam warahmatullahi wabarokatuh*
+    if (!text) return conn.reply(m.chat, 'Contoh penggunaan:\n' + usedPrefix + 'assalamualaikum\n' + usedPrefix + 'salam', m)
 
+    let caption = `*Waalaikummussalam warahmatullahi wabarokatuh*\n\n_📚 Baca yang dibawah ya!_ "Orang yang mengucapkan salam seperti ini maka ia mendapatkan 30 pahala, kemudian, orang yang dihadapan atau mendengarnya membalas dengan kalimat yang sama yaitu “Wa'alaikum salam warahmatullahi wabarakatuh” atau ditambah dengan yang lain (waridhwaana). Artinya selain daripada do'a selamat juga meminta pada Allah SWT."`
 
-_📚 Baca yang dibawah ya!_
-"Orang yang mengucapkan salam seperti ini maka ia mendapatkan 30 pahala, kemudian, orang yang dihadapan atau mendengarnya membalas dengan kalimat yang sama yaitu “Wa'alaikum salam warahmatullahi wabarakatuh” atau ditambah dengan yang lain (waridhwaana). Artinya selain daripada do'a selamat juga meminta pada Allah SWT"
-`
-
-    conn.reply(m.chat, caption, m)
+    conn.reply(m.chat, caption, m, MessageType.text)
 }
+
 handler.customPrefix = /^(assalam(ualaikum)?|(salamualaiku|(sa(lamu|m)liku|sala))m)$/i
 handler.command = new RegExp
 
-export default handler
+module.exports = handler
