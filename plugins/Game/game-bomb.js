b9b71a36fae4d166c6333789316cee7781d85cf5
M.U.F.A.R <132034817+Pkok1024@users.noreply.github.com>
2024-04-19 12:30:42 +0700

    comment

--- plugins/Game/game-bomb.js
+++ plugins/Game/game-bomb.js
@@ -1,12 +1,20 @@
-let handler = async (m, {
+// Define the bomb handler function
+const handler = async (m, {
     conn
 }) => {
+    // Initialize the bomb object for the current chat if it doesn't exist
     conn.bomb = conn.bomb || {};
-    let id = m.chat,
-        timeout = 180000;
+    const id = m.chat, // Get the chat ID
+        timeout = 180000; // Set the timeout duration in milliseconds
+
+    // Return if the chat ID is already in the bomb object
     if (id in conn.bomb) return conn.reply(m.chat, '*^ sesi ini belum selesai!*', conn.bomb[id][0]);
+
+    // Initialize the bomb array with emojis and numbers
     const bom = ['💥', '✅', '✅', '✅', '✅', '✅', '✅', '✅', '✅'].sort(() => Math.random() - 0.5);
     const number = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣'];
+
+    // Map the bomb array to an array of objects containing emot, number, position, state, and player properties
     const array = bom.map((v, i) => ({
         emot: v,
         number: number[i],
@@ -14,30 +22,11 @@ let handler = async (m, {
         state: false,
         player: m.sender
     }));
+
+    // Prepare the initial message to be sent to the user
     let teks = `乂  *B O M B*\n\nKirim angka *1* - *9* untuk membuka *9* kotak nomor di bawah ini :\n\n`;
     for (let i = 0; i < array.length; i += 3) teks += array.slice(i, i + 3).map(v => v.state ? v.emot : v.number).join('') + '\n';
     teks += `\nTimeout : [ *${((timeout / 1000) / 60)} menit* ]\nApabila mendapat kotak yang berisi bom maka point akan di kurangi.`;
-    let msg = await conn.reply(m.chat, teks, m);
-    let {
-        key
-    } = msg
 
-    let v;
-    conn.bomb[id] = [
-        msg,
-        array,
-        setTimeout(() => {
-            v = array.find(v => v.emot == '💥');
-            if (conn.bomb[id]) conn.reply(m.chat, `*Waktu habis!*, Bom berada di kotak nomor ${v.number}.`, conn.bomb[id][0].key);
-            delete conn.bomb[id];
-        }, timeout),
-        key
-    ];
-
-};
-
-handler.help = ["bomb"];
-handler.tags = ["game"];
-handler.command = /^(bomb)$/i;
-
-export default handler;
+    // Send the initial message and store the message object
+    let msg = await conn.reply(m.chat, teks, m
