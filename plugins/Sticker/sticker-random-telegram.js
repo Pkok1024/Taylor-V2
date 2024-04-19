25770464c49f8709b6a233b5a50de3ece7af2a1e
M.U.F.A.R <132034817+Pkok1024@users.noreply.github.com>
2024-04-19 12:30:18 +0700

    comment

--- plugins/Sticker/sticker-random-telegram.js
+++ plugins/Sticker/sticker-random-telegram.js
@@ -1,11 +1,14 @@
-import wibusoft from 'wibusoft'
-import fetch from 'node-fetch'
+import wibusoft from 'wibusoft' // Importing wibusoft module
+import fetch from 'node-fetch' // Importing fetch module
+
+// Defining the handler function which handles the command
 let handler = async (m, {
-    text,
-    command,
-    usedPrefix,
-    conn
+    text, // The text input by the user
+    command, // The command used by the user
+    usedPrefix, // The prefix used by the user
+    conn // The connection object
 }) => {
+    // Defining an array of valid input strings
     var list_input = [
         "awoawo",
         "chat",
@@ -28,27 +31,21 @@ let handler = async (m, {
         "sponsbob",
         "tyni"
     ]
+
+    // Defining an error message to be thrown if the input is invalid
     var salah_input = "*Example:*\n" + usedPrefix + command + " tyni \n*[ Daftar Stiker ]*\n\n" + await ArrClean(list_input)
+
+    // Checking if the input is not in the list of valid inputs
     if (!list_input.includes(text)) throw salah_input
+
+    // Handling the command
     try {
+        // Defining the URL of the API endpoint to be called
         let res = 'https://api.zeeoneofc.my.id/api/telegram-sticker/' + text + '?apikey=dhmDlD5x'
+
+        // Sending a "wait" message to the user
         m.reply(wait)
+
+        // Calling the makeSticker function from the wibusoft module
         let out = await wibusoft.tools.makeSticker(res, {
-            author: author,
-            pack: packname,
-            keepScale: true
-        })
-        await m.reply(out)
-    } catch (e) {
-        throw eror
-    }
-}
-handler.help = ["telegramsticker"]
-handler.tags = ['sticker']
-handler.command = ["telegramsticker"]
-
-export default handler
-
-function ArrClean(str) {
-    return str.map((v, index) => ++index + ". " + v).join('\r\n')
-}
+            author: author, // The author of the
