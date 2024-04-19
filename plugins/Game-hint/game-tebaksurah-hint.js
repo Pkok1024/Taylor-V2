cfb1309e1c2389d3e3d1d44e50eadb37aa459b55
M.U.F.A.R <132034817+Pkok1024@users.noreply.github.com>
2024-04-19 12:29:55 +0700

    improve

--- plugins/Game-hint/game-tebaksurah-hint.js
+++ plugins/Game-hint/game-tebaksurah-hint.js
@@ -1,14 +1,19 @@
-let handler = async (m, {
-    conn
-}) => {
-    conn.tebaksurah = conn.tebaksurah ? conn.tebaksurah : {}
-    let id = m.chat
-    if (!(id in conn.tebaksurah)) throw false
-    let json = conn.tebaksurah[id][1]
-    conn.reply(m.chat, '```' + json.surah.englishName.replace(/[AIUEOaiueo]/ig, '_') + '```', m)
+let handler = async (m, { conn }) => {
+  try {
+    conn.tebaksurah = conn.tebaksurah || {};
+    let id = m.chat;
+    if (!(id in conn.tebaksurah)) throw false;
+    let json = conn.tebaksurah[id][1];
+    if (!json || !json.surah || !json.surah.englishName) throw false;
+    let result = json.surah.englishName.replace(/[AIUEOaiueo]/ig, '_');
+    if (!result) throw false;
+    await conn.reply(m.chat, '```' + result + '```', m);
+  } catch (e) {
+    console.error(e);
+    throw false;
+  }
 }
-handler.command = /^hsur$/i
+handler.command = /^hsur$/i;
+handler.limit = true;
 
-handler.limit = true
-
-export default handler
+export default handler;
