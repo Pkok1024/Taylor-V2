0d652fb478cd71ccdae4c501eadfbde4de99a599
M.U.F.A.R <132034817+Pkok1024@users.noreply.github.com>
2024-04-19 12:29:36 +0700

    improve

--- plugins/Others/vote-hapus.js
+++ plugins/Others/vote-hapus.js
@@ -1,20 +1,19 @@
-let handler = async (m, {
-    conn,
-    usedPrefix,
-    isAdmin,
-    isOwner
-}) => {
-    if (m.isGroup) {
-        if (!(isAdmin || isOwner)) return dfail('admin', m, conn)
-    }
-    let id = m.chat
-    conn.vote = conn.vote ? conn.vote : {}
-    if (!(id in conn.vote)) await conn.reply(m.chat, `Tidak ada voting digrup ini!`, m)
-    delete conn.vote[id]
-    m.reply(`Berhasil!`)
+const handler = async (m, { conn, usedPrefix, isAdmin, isOwner }) => {
+  if (m.isGroup) {
+    if (!(isAdmin && isOwner)) return conn.reply(m.chat, 'You must be an admin and owner to use this command!', m)
+  }
+
+  const id = m.chat
+  const { vote } = conn
+
+  if (!vote || !vote[id]) return conn.reply(m.chat, 'There is no voting in this chat!', m)
+
+  delete vote[id]
+  m.reply('Successfully deleted the vote!')
 }
-handler.help = ['hapusvote']
+
+handler.help = ['deletevote']
 handler.tags = ['vote']
 handler.command = /^(delete|hapus|-)vote$/i
 
-export default handler
+module.exports = handler
