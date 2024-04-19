adcf9b9115d3b998decc5bab0bea7a22d95ad184
M.U.F.A.R <132034817+Pkok1024@users.noreply.github.com>
2024-04-19 12:29:30 +0700

    improve

--- plugins/Others/sound-ml.js
+++ plugins/Others/sound-ml.js
@@ -1,9 +1,8 @@
 import axios from "axios"
-import fetch from "node-fetch"
-import cheerio from "cheerio"
 import {
     JSDOM
 } from "jsdom"
+import path from "path"
 
 let handler = async (m, {
     conn,
@@ -14,43 +13,30 @@ let handler = async (m, {
 }) => {
     if (!text) throw "input text"
     try {
-        if (command == "mlsounden") {
-            await m.reply(wait)
-            let res = await MLSound("en", text)
+        if (command == "mlsounden" || command == "mlsoundid") {
+            await m.reply(global.wait)
+            let res = await MLSound(command, text)
             let rdm = res[Math.floor(Math.random() * res.length)];
-            await conn.sendMessage(m.chat, {
-                audio: {
-                    url: rdm
-                },
-                seconds: fsizedoc,
+            let {
+                data: {
+                    headers
+                }
+            } = await axios.head(rdm)
+            let audio = {
+                audio: rdm,
+                seconds: Math.ceil(parseInt(headers["content-length"]) / 44100),
                 ptt: true,
                 mimetype: "audio/mpeg",
-                fileName: rdm.split("/")[4] + ".mp3",
+                fileName: path.basename(rdm),
                 waveform: [100, 0, 100, 0, 100, 0, 100]
-            }, {
+            }
+            await conn.sendMessage(m.chat, audio, {
                 quoted: m
             })
         }
-        if (command == "mlsoundid") {
-            await m.reply(wait)
-            let res = await MLSound("id", text)
-            let rdm = res[Math.floor(Math.random() * res.length)];
-            await conn.sendMessage(m.chat, {
-                audio: {
-                    url: rdm
-                },
-                seconds: fsizedoc,
-                ptt: true,
-                mimetype: "audio/mpeg",
-                fileName: rdm.split("/")[7] + ".mp3",
-                waveform: [100, 0, 100, 0, 100, 0, 100]
-            }, {
-                quoted: m
-            })
-        }
-
     } catch (e) {
-        throw eror
+        console.error(e)
+        await m.reply(global.eror)
     }
 }
 handler.help = ["mlsounden", "mlsoundid"]
@@ -63,16 +49,16 @@ export default handler
 async function MLSound(tema, query) {
     let res
     if (tema == "id") {
-        res = await fetch("https://mobile-legends.fandom.com/wiki/" + query + "/Audio/id")
+        res = await axios.get("https://mobile-legends.fandom.com/wiki/" + query + "/Audio/id")
     }
     if (tema == "en") {
-        res = await fetch("https://mobilelegendsbuild.com/sound/" + query)
+        res = await axios.get("https://mobilelegendsbuild.com/sound/" + query)
     }
-    let html = await res.text()
+    let html = res.data
     let dom = new JSDOM(html)
-    var totals = dom.window.document.getElementsByTagName("audio");
     let audio = []
-    for (var i = 0; i < totals.length; i++) {
+    let totals = dom.window.document.getElementsByTagName("audio")
+    for (let i = 0; i < totals.length; i++) {
         audio.push(totals[i].getAttribute("src"))
     }
     return audio
