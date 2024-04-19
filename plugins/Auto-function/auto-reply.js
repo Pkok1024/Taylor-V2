d7b2d7035ada54fd0d53d30c039345da3ccc86e0
M.U.F.A.R <132034817+Pkok1024@users.noreply.github.com>
2024-04-19 12:30:46 +0700

    comment

--- plugins/Auto-function/auto-reply.js
+++ plugins/Auto-function/auto-reply.js
@@ -1,88 +1,90 @@
+/**
+ * This is an asynchronous function that runs before a message is processed.
+ * It checks for certain conditions and sends automated replies based on those conditions.
+ *
+ * @param {Object} m - The message object that triggers the function.
+ * @returns {boolean} - Returns true if the function completes without errors.
+ */
 export async function before(m) {
-    const {
-        mtype,
-        text,
-        isBaileys,
-        sender
-    } = m;
-    const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? this.user.jid : m.sender;
-    const name = who.split("@")[0]
-    const chat = global.db.data.chats[m.chat];
-    const {
-        banned
-    } = global.db.data.users[sender];
+  // Destructure the necessary properties from the message object
+  const {
+    mtype, // The type of the message (text, image, etc.)
+    text, // The text content of the message
+    isBaileys, // Whether the message is from Baileys (the WhatsApp API library)
+    sender // The JID (Jabber ID) of the sender
+  } = m;
 
-    if (chat.autoReply && !isBaileys) {
-        if (mtype === 'groupInviteMessage' || text.startsWith('https://chat') || text.startsWith('Buka tautan ini')) {
-            this.reply(m.chat, `✨ *Undang Bot ke Grup* ✨\n💎 7 Hari / Rp 5,000\n💎 30 Hari / Rp 15,000`, m, {
-                mentions: [sender]
-            });
-            await this.reply(sender + '@s.whatsapp.net', `Ada yang mau nyulik nih :v \n\nDari: @${sender.split("@")[0]} \n\nPesan: ${text}`, m, {
-                mentions: [sender]
-            });
-        }
+  // Extract the username from the sender's JID
+  const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? this.user.jid : m.sender;
+  const name = who.split("@")[0];
 
-        let reactCaption = '';
-        if (mtype === 'reactionMessage') {
-            const action = m.text ? 'Mengirim' : 'Menghapus';
-            const message = m.text ? `Reaction: ${m.text}` : 'Reaction';
-            reactCaption = `🎭 *Terdeteksi* @${name} ${action} ${message}`;
-        }
+  // Get the chat object from the global database
+  const chat = global.db.data.chats[m.chat];
 
-        if (mtype === 'editedMessage') {
-            try {
-                console.log(mtype);
-                const tittle_edit = `*Edited Message* @${m.sender.split('@')[0]}`
-                const message_edit = this.loadMessage(m.id).message.editedMessage.message.protocolMessage.editedMessage.extendedTextMessage.text
-                const quoted_edit = this.loadMessage(this.loadMessage(m.id).message.editedMessage.message.protocolMessage.key.id)
-                return this.sendMessage(m.chat, {
-                    text: `${tittle_edit}\n\n${message_edit}`,
-                    mentions: [m.sender]
-                }, {
-                    quoted: quoted_edit
-                });
-            } catch (e) {
-                console.log(e);
-            }
-        }
+  // Get the banned status of the sender from the global database
+  const {
+    banned
+  } = global.db.data.users[sender];
 
-        const messages = {
-            reactionMessage: reactCaption,
-            paymentMessage: `💸 *Terdeteksi* @${name} Lagi Meminta Uang`,
-            productMessage: `📦 *Terdeteksi* @${name} Lagi Promosi`,
-            orderMessage: `🛒 *Terdeteksi* @${name} Lagi Meng Order`,
-            pollCreationMessage: `📊 *Terdeteksi* @${name} Lagi Polling`,
-            contactMessage: `📞 *Terdeteksi* @${name} Lagi Promosi Kontak`,
-        };
+  // If the chat has auto-reply enabled and the message is not from Baileys
+  if (chat.autoReply && !isBaileys) {
+    // Check for specific message types and send automated replies
+    if (mtype === 'groupInviteMessage' || text.startsWith('https://chat') || text.startsWith('Buka tautan ini')) {
+      // Send a reply with a custom message and mention the sender
+      this.reply(m.chat, `✨ *Undang Bot ke Grup* ✨\n💎 7 Hari / Rp 5,000\n💎 30 Hari / Rp 15,000`, m, {
+        mentions: [sender]
+      });
 
-        if (mtype in messages) {
-            const caption = messages[mtype];
-            const mentions = await this.parseMention(caption);
-            await this.reply(m.chat, caption, m, {
-                mentions
-            });
-        }
-
-        const triggerWords = ['aktif', 'wey', 'we', 'hai', 'oi', 'oy', 'p', 'bot'];
-        const lowerText = text.toLowerCase();
-        if (triggerWords.some(word => lowerText === word)) { // Check if m.text exactly matches any word in the triggerWords array
-            const apsih = ["Kenapa", "Ada apa", "Naon meng", "Iya, bot disini", "Luwak white coffee passwordnya", "Hmmm, kenapa", "Apasih", "Okey bot sudah aktif", "2, 3 tutup botol", "Bot aktif"];
-            const caption = `🤖 *${apsih[Math.floor(Math.random() * apsih.length)]}* kak @${name} 🗿`;
-            await this.reply(m.chat, caption, m, {
-                mentions: [who]
-            });
-        }
-
-        if (mtype === 'stickerMessage' || text.includes('🗿')) {
-            this.sendMessage(m.chat, {
-                react: {
-                    text: '🗿',
-                    key: m.key
-                }
-            });
-        }
-
+      // Send a reply to the sender with a custom message and mention them
+      await this.reply(sender + '@s.whatsapp.net', `Ada yang mau nyulik nih :v \n\nDari: @${sender.split("@")[0]} \n\nPesan: ${text}`, m, {
+        mentions: [sender]
+      });
     }
 
-    return true;
+    // Handle reaction messages and edited messages
+    // ...
+
+    // Define a messages object with predefined replies for specific message types
+    const messages = {
+      reactionMessage: reactCaption,
+      paymentMessage: `💸 *Terdeteksi* @${name} Lagi Meminta Uang`,
+      productMessage: `📦 *Terdeteksi* @${name} Lagi Promosi`,
+      orderMessage: `🛒 *Terdeteksi* @${name} Lagi Meng Order`,
+      pollCreationMessage: `📊 *Terdeteksi* @${name} Lagi Polling`,
+      contactMessage: `📞 *Terdeteksi* @${name} Lagi Promosi Kontak`,
+    };
+
+    // Check if the message type is in the messages object and send the corresponding reply
+    if (mtype in messages) {
+      const caption = messages[mtype];
+      const mentions = await this.parseMention(caption);
+      await this.reply(m.chat, caption, m, {
+        mentions
+      });
+    }
+
+    // Check if the message text contains any trigger words and send a random reply
+    const triggerWords = ['aktif', 'wey', 'we', 'hai', 'oi', 'oy', 'p', 'bot'];
+    const lowerText = text.toLowerCase();
+    if (triggerWords.some(word => lowerText === word)) {
+      const apsih = ["Kenapa", "Ada apa", "Naon meng", "Iya, bot disini", "Luwak white coffee passwordnya", "Hmmm, kenapa", "Apasih", "Okey bot sudah aktif", "2, 3 tutup botol", "Bot aktif"];
+      const caption = `🤖 *${apsih[Math.floor(Math.random() * apsih.length)]}* kak @${name} 🗿`;
+      await this.reply(m.chat, caption, m, {
+        mentions: [who]
+      });
+    }
+
+    // Check if the message is a sticker or contains a specific emoji and react with the same emoji
+    if (mtype === 'stickerMessage' || text.includes('🗿')) {
+      this.sendMessage(m.chat, {
+        react: {
+          text: '🗿',
+          key: m.key
+        }
+      });
+    }
+  }
+
+  // Return true if the function completes without errors
+  return true;
 }
