182bc19701f1c78ba06464fbf7f832f6bb940cb5
M.U.F.A.R <132034817+Pkok1024@users.noreply.github.com>
2024-04-19 12:29:28 +0700

    improve

--- plugins/Downloader/downloader-zippy.js
+++ plugins/Downloader/downloader-zippy.js
@@ -3,6 +3,8 @@ import {
     szippydl
 } from '../../lib/scraper/scrape.js'
 
+const MAX_FILE_SIZE = 100 * 1024 * 1024 // 100 MB
+
 let handler = async (m, {
     conn,
     args,
@@ -10,18 +12,35 @@ let handler = async (m, {
     command
 }) => {
     if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
-    let res = await szippydl(args[0])
-    let done = `*title:* ${res.title}
-*extension:* ${res.extension}
-*filesize:* ${res.filesize}
-*upload:* ${res.upload}
-*link:* ${res.link}`
-    if (res.link) return conn.sendFile(m.chat, res.link, '', done, m)
-    else throw eror
+
+    try {
+        let res = await szippydl(args[0])
+        let {
+            title,
+            extension,
+            filesize,
+            upload,
+            link
+        } = res
+
+        if (!extension.includes('mp4')) throw new Error('Invalid file type. Only mp4 files are supported.')
+        if (filesize > MAX_FILE_SIZE) throw new Error(`File size is too big. Maximum file size is ${MAX_FILE_SIZE / (1024 * 1024)} MB.`)
+
+        let done = `*title:* ${title}
+*extension:* ${extension}
+*filesize:* ${filesize}
+*upload:* ${upload}
+*link:* ${link}`
+
+        if (link) return conn.sendFile(m.chat, link, '', done, m)
+    } catch (err) {
+        throw err.message
+    }
 }
-handler.help = ['zippyshare'].map(v => v + ' <url>')
+handler.help = ['zippy(share)?(ser)?(sher)?(sare)?'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 
 handler.command = /^(zippy(share)?(ser)?(sher)?(sare)?)$/i
 
 export default handler
+
