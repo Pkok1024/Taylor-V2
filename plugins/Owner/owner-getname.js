2c4aea7afae77ee80fa17fa972108d52b608801d
M.U.F.A.R <132034817+Pkok1024@users.noreply.github.com>
2024-04-19 12:29:44 +0700

    comment

--- plugins/Owner/owner-getname.js
+++ plugins/Owner/owner-getname.js
@@ -1,25 +1,48 @@
+// Define an asynchronous handler function that takes in 'm' (message object),
+// 'conn' (connection object), and 'command' (command string) as parameters.
 let handler = async (m, {
-    conn,
-    command
+    conn, // Connection object
+    command // Command string
 }) => {
     try {
-        let who
-        if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
-        else who = m.quoted.sender ? m.quoted.sender : m.sender
+        // Check if the message is from a group chat
+        if (m.isGroup) {
+            // If it is, get the first mentioned JID or the quoted message sender's JID
+            who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
+        } else {
+            // If it's not, get the quoted message sender's JID or the sender's JID
+            who = m.quoted.sender ? m.quoted.sender : m.sender
+        }
+
+        // Get the name of the user with the specified JID
         let name = await conn.getName(who)
+
+        // Reply to the message with the user's name
         m.reply(name)
-    } catch {
+    } catch (err) {
         try {
-            let who = m.quoted ? m.quoted.sender : m.sender
+            // If there was an error, try getting the name of the quoted message sender
+            who = m.quoted ? m.quoted.sender : m.sender
             let name = await conn.getName(who)
             m.reply(name)
         } catch {
+            // If there's still an error, throw an error message
             throw `sorry gk bisa coba yang lain⍨`
         }
     }
 }
+
+// Set the help message for the handler
 handler.help = ['getname <@tag/reply>']
+
+// Set the tags for the handler
 handler.tags = ['owner']
+
+// Set the command regular expression for the handler
 handler.command = /^(get)?name?a?$/i
+
+// Set the owner-only status for the handler
 handler.owner = true
+
+// Export the handler as the default export
 export default handler
